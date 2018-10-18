9c9384a360977e374b65237c3a52d6067fb47c79
PppBr <bolopeng1991@gmail.com>
2018-10-18 11:16:29 -0500

    Add NdArray Mean function

---
+++ src/NumSharp/Extensions/NdArray.Mean.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumSharp.Extensions
+{
+    public static partial class NDArrayExtensions
+    {
+        public static NDArray<double> Mean(this NDArray<NDArray<double>> np, int axis = -1)
+        {
+            var mean = new NDArray<double>();
+            // axis == -1: DEFAULT; to compute the mean of the flattened array.
+            if (axis == -1)
+            {
+                double sum = 0;
+                for (int d = 0; d < np.Length; d++)
+                {
+                    for (int p = 0; p < np.Data[0].Length; p++) {
+                        sum += np.Data[d].Data[p];
+                    }
+                }
+                mean.Data.Add(sum / (np.NDim * np.Data[0].Length));
+            }
+            // to compute mean by compressing row and row
+            else if (axis == 0)
+            {
+                double[] sumVec = new double[np.Data[0].Length];
+                for (int d = 0; d < np.Length; d++)
+                {
+                    for (int p = 0; p < np.Data[0].Length; p++)
+                    {
+                        sumVec[p] += np.Data[d].Data[p];
+                    }
+                }
+                for (int d = 0; d < np.Data[0].Length; d++) {
+                    mean.Data.Add(sumVec[d] / np.Length);
+                }
+            }
+            else if (axis == 1)
+            {
+                for (int d = 0; d < np.Length; d++)
+                {
+                    double rowSum = 0;
+                    for (int p = 0; p < np.Data[0].Length; p++)
+                    {
+                        rowSum += np.Data[d].Data[p];
+                    }
+                    mean.Data.Add(rowSum / np.Data[0].Length);
+                }
+            }
+
+            return mean;
+        }
+    }
+}
---
+++ test/NumSharp.UnitTest/Extensions/NdArray.Mean.Test.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp.Extensions;
+using System.Linq;
+
+namespace NumSharp.UnitTest.Extensions
+{
+    [TestClass]
+    public class NdArrayMeanTest
+    {
+        [TestMethod]
+        public void Mean()
+        {
+            var series1 = new NDArray<double>();
+            series1.Data = new double[] { 1, 2 };
+
+            var series2 = new NDArray<double>();
+            series2.Data = new double[] { 3, 4 };
+
+            var np = new NDArray<NDArray<double>>();
+            np.Data.Add(series1);
+            np.Data.Add(series2);
+
+            double[] expectedResultDefault = new double[] { 2.5 };
+            double[] expectedResult0 = new double[] { 2, 3 };
+            double[] expectedResult1 = new double[] { 1.5, 3.5 };
+
+            Assert.IsTrue(Enumerable.SequenceEqual(np.Mean().Data, expectedResultDefault));
+            Assert.IsTrue(Enumerable.SequenceEqual(np.Mean(0).Data, expectedResult0));
+            Assert.IsTrue(Enumerable.SequenceEqual(np.Mean(1).Data, expectedResult1));
+        }
+    }
+}
