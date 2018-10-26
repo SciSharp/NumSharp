00c1d034c6a5661f4184b1a0d66522ee70ac3bc5
dotchris90 <dotchris@online.de>
2018-10-26 14:29:22 +0200

    Add operator "+" and "-" for NDArray 1D

---
+++ test/NumSharp.UnitTest/Operations/NDArray.Substraction.Test.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp.Extensions;
+using System.Linq;
+
+namespace NumSharp.UnitTest
+{
+    [TestClass]
+    public class NDArraySubstractionTest
+    {
+        
+        [TestMethod]
+        public void  DoubleTwo1D()
+        {
+            var np1 = new NDArray<double>().Array(new double[]{3,5,7});
+            var np2 = new NDArray<double>().Array(new double[]{1,3,4});
+
+            var np3 = np1 - np2;
+
+            Assert.IsTrue(Enumerable.SequenceEqual(new double[]{2,2,3},np3.Data));
+
+        }
+        [TestMethod]
+        public void  ComplexTwo1D()
+        {
+            var np1 = new NDArray<Complex>().Array(new Complex[]{new Complex(6,8),new Complex(10,12)});
+            var np2 = new NDArray<Complex>().Array(new Complex[]{new Complex(5,6),new Complex(7,8)});
+
+            var np3 = np1 - np2;
+
+            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[]{new Complex(1,2),new Complex(3,4)},np3.Data));
+
+        }
+        
+    }
+}
