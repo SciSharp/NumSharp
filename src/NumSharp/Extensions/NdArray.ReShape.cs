a71239f6d1ffcb5d247c940b994dbed77148a998
fdncred <fdncred@gmail.com>
2018-11-01 10:28:30 -0500

    1st attempt to allow -1 for reshape.

--- src/NumSharp/Extensions/NdArray.ReShape.cs
+++ src/NumSharp/Extensions/NdArray.ReShape.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Text;
+using System.Linq;
 
 namespace NumSharp.Extensions
 {
@@ -14,9 +15,84 @@ namespace NumSharp.Extensions
         /// <returns></returns>
         public static NDArray<T> ReShape<T>(this NDArray<T> np, params int[] shape)
         {
-            np.Shape = shape;
+            var count = shape.Length;
+            var idx = FindNegativeIndex(shape);
+            if (idx == -1)
+                np.Shape = shape;
+            else
+                np.Shape = CalculateNegativeShape(idx, np.Shape, shape);
+
+            //np.Shape = newShape;
 
             return np;
+        }
+
+        private static int FindNegativeIndex(params int[] shape)
+        {
+            var count = shape.Length;
+            var negOne = false;
+            var indexOfNegOne = -1;
+            for (int i = 0; i < count; i++)
+            {
+                if (shape[i] == -1)
+                {
+                    if (negOne)
+                        throw new ArgumentException("Only allowed to pass one shape dimension as -1");
+
+                    negOne = true;
+                    indexOfNegOne = i;
+                }
+            }
+
+            return indexOfNegOne;
+        }
+
+        private static IList<int> CalculateNegativeShape(int negativeIndex, IList<int> currentShape, params int[] shapeParams)
+        {
+            var currentShapeCount = currentShape.Count;
+            var shapeParamCount = shapeParams.Length;
+            var newShape = new List<int>();
+            var curShapeVolume = currentShape.Aggregate((x, y) => x * y);
+            if (negativeIndex > -1)
+            {
+                int x = shapeParams[0];
+                int y = 0;
+                if (shapeParamCount >= 1)
+                    y = shapeParams[1];
+                if (shapeParamCount > 2)
+                    throw new ArgumentException("We cannot currently handle reshapes of more than 2 dimensions");
+
+                if (negativeIndex == 0 && shapeParamCount == 2)
+                {
+                    var mod = curShapeVolume % y == 0;
+                    if (!mod)
+                        throw new ArgumentException($"Wrong Reshape. {curShapeVolume} is not evenly divisible by {y}");
+                    else
+                    {
+                        var a = curShapeVolume / y;
+                        var b = y;
+                        newShape.Add(a);
+                        newShape.Add(b);
+                    }
+                }
+                else if (negativeIndex == 1 && shapeParamCount == 2)
+                {
+                    var mod = curShapeVolume % x == 0;
+                    if (!mod)
+                        throw new ArgumentException($"Wrong Reshape. {curShapeVolume} is not evenly divisible by {x}");
+                    else
+                    {
+                        var a = x;
+                        var b = curShapeVolume / x;
+                        newShape.Add(a);
+                        newShape.Add(b);
+                    }
+                }
+            }
+            else
+                return currentShape;
+
+            return newShape;
         }
     }
 }
--- test/NumSharp.UnitTest/Extensions/NdArray.ReShape.Test.cs
+++ test/NumSharp.UnitTest/Extensions/NdArray.ReShape.Test.cs
@@ -36,5 +36,52 @@ namespace NumSharp.UnitTest.Extensions
             var np = new NDArray<int>();
             np.ARange(1024 * 1024);
         }
+
+        [TestMethod]
+        public void PerformaceBitmapSimulation()
+        {
+            var npRealWorldBitmap = new NDArray<byte>();
+            npRealWorldBitmap.ARange(2081 * 2531);
+            npRealWorldBitmap.ReShape(2531, 2081);
+        }
+
+        [TestMethod]
+        public void ReshapeNegative()
+        {
+            var np = new NDArray<int>();
+            np.ARange(12);
+            np.ReShape(-1, 2);
+            Assert.IsTrue(np.Shape[0] == 6);
+            Assert.IsTrue(np.Shape[1] == 2);
+
+            np.ARange(12);
+            np.ReShape(2, -1);
+            Assert.IsTrue(np.Shape[0] == 2);
+            Assert.IsTrue(np.Shape[1] == 6);
+
+            np.ARange(12);
+            np.ReShape(1, 3, 4);
+            np.ReShape(-1, 3);
+            Assert.IsTrue(np.Shape[0] == 4);
+            Assert.IsTrue(np.Shape[1] == 3);
+
+            np.ARange(12);
+            np.ReShape(1, 3, 4);
+            np.ReShape(3, -1);
+            Assert.IsTrue(np.Shape[0] == 3);
+            Assert.IsTrue(np.Shape[1] == 4);
+
+            np.ARange(100 * 100 * 3);
+            np.ReShape(100, 100, 3);
+            np.ReShape(-1, 3);
+            Assert.IsTrue(np.Shape[0] == 10000);
+            Assert.IsTrue(np.Shape[1] == 3);
+
+            np.ARange(15801033);
+            np.ReShape(2531, 2081, 3);
+            np.ReShape(-1, 3);
+            Assert.IsTrue(np.Shape[0] == 5267011);
+            Assert.IsTrue(np.Shape[1] == 3);
+        }
     }
 }
