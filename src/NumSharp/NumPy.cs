ee10e36415b3318b726030c6a6c82f8f47e7ddae
Esther Hu <estherhu2012@gmail.com>
2018-11-03 09:31:08 -0500

    add NumPy.array API and unit test for 1 and 2 dim.

--- src/NumSharp/NumPy.cs
+++ src/NumSharp/NumPy.cs
@@ -70,5 +70,36 @@ namespace NumSharp
                     throw new NotImplementedException();
             }
         }
+
+        public NDArray<T> array(T[] data)
+        {
+            var n = new NDArray<T>();
+            n.Data = data;
+            n.Shape[0] = data.Length;
+
+            return n;
+        }
+
+        public NDArray<T> array(T[][] data)
+        {
+            int size = data.Length * data[0].Length;
+            var all = new T[size];
+
+            int idx = 0;
+            for (int row = 0; row < data.Length; row++)
+            {
+                for (int col = 0; col < data[row].Length; col++)
+                {
+                    all[idx] = data[row][col];
+                    idx++;
+                }
+            }
+
+            var n = new NDArray<T>();
+            n.Data = all;
+            n.Shape = new List<int> { data.Length, data[0].Length };
+
+            return n;
+        }
     }
 }
