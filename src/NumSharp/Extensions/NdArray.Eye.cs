9a4f9ade76a9958cd7a8c2d623fa502fd089ee7c
dotchris90 <dotchris@online.de>
2018-11-04 11:59:43 +0100

    Add eye method for 2D arrays (not higher dimension)

---
+++ src/NumSharp/Extensions/NdArray.Eye.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace NumSharp.Extensions
+{
+    public static partial class NDArrayExtensions
+    {
+        public static NDArray<int> Eye(this NDArray<int> np,int dim, int diagonalIndex = 0)
+        {
+            int noOfDiagElement = dim - Math.Abs(diagonalIndex);
+
+            if((np.NDim == 1) && (dim != 1))
+            {
+                np.Zeros(dim,dim);
+            }
+            else 
+            {
+                np.Zeros(np.Shape.ToArray());
+            }
+            
+            np.Data = new int[np.Size];
+
+            for(int idx = 0; idx < noOfDiagElement;idx++ )
+             {
+                 np.Data[diagonalIndex + idx + idx * np.Shape[1]] = 1;
+             }
+            
+            return np;
+        }
+        public static NDArray<double> Eye(this NDArray<double> np,int dim, int diagonalIndex = 0)
+        {
+            int noOfDiagElement = dim - Math.Abs(diagonalIndex);
+
+            if((np.NDim == 1) && (dim != 1))
+            {
+                np.Zeros(dim,dim);
+            }
+            else 
+            {
+                np.Zeros(np.Shape.ToArray());
+            }
+            
+            np.Data = new double[np.Size];
+
+            if (diagonalIndex >= 0)
+            {
+                for(int idx = 0; idx < noOfDiagElement;idx++ )
+                {
+                     np.Data[diagonalIndex + idx + idx * np.Shape[1]] = 1;
+                }
+            }
+            else
+            {
+                for(int idx = 0; idx < noOfDiagElement;idx++ )
+                {
+                     np.Data[(-1)*diagonalIndex * dim + idx + idx * np.Shape[1]] = 1;
+                }
+            }
+
+            return np;
+        }
+    }
+}
