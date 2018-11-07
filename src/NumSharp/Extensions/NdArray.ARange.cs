ce6fc318fde05ff5a15e78eb95a54a57bc04d63e
Oceania2018 <haiping008@gmail.com>
2018-11-07 16:08:36 -0600

    fix volume bug for arange.

--- src/NumSharp/Extensions/NdArray.ARange.cs
+++ src/NumSharp/Extensions/NdArray.ARange.cs
@@ -10,20 +10,21 @@ namespace NumSharp.Extensions
     {
         public static NDArray<int> ARange(this NDArray<int> np,int stop, int start = 0, int step = 1)
         {
+            var list = new int[(int)Math.Ceiling((stop - start + 0.0) / step)];
             int index = 0;
 
-            np.Data = Enumerable.Range(start,stop - start)
-                                .Where(x => index++ % step == 0)
-                                .ToArray();
+            for (int i = start; i < stop; i += step)
+                list[index++] = i;
 
-            np.Shape = new Shape(new int[] { stop });
+            np.Data = list;
+            np.Shape = new Shape(list.Length);
 
             return np;
         }
 
         public static NDArray<double> ARange(this NDArray<double> np, int stop, int start = 0, int step = 1)
         {
-            var list = new double[(stop - start) / step];
+            var list = new double[(int)Math.Ceiling((stop - start + 0.0) / step)];
             int index = 0;
 
             for (int i = start; i < stop; i += step)
