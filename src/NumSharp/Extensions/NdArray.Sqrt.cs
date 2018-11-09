120860bfce141454092f28546363ba367e645110
Vunb <nhubaovu@gmail.com>
2018-11-09 14:55:11 +0700

    add numsharp sqrt

---
+++ src/NumSharp/Extensions/NdArray.Sqrt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumSharp.Extensions
+{
+    public static partial class NDArrayExtensions
+    {
+        public static NDArray<double> Sqrt(this NDArray<double> np)
+        {
+            for (int i = 0; i < np.Data.Length; i++)
+            {
+                np[i] = Math.Sqrt(np[i]);
+            }
+
+            return np;
+        }
+    }
+}
---
+++ test/NumSharp.UnitTest/Extensions/NdArray.Sqrt.Test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp.Extensions;
+using System.Linq;
+
+namespace NumSharp.UnitTest.Extensions
+{
+    [TestClass]
+    public class NDArraySqrtTest
+    {
+        [TestMethod]
+        public void SqrtTest()
+        {
+            var np = new NDArray<double>().ARange(3);
+            np[0] = 1;
+            np[1] = 4;
+            np[2] = 9;
+            Assert.IsTrue(Enumerable.SequenceEqual(np.Sqrt().Data, new double[] { 1, 2, 3 }));
+        }
+    }
+}
