e2293d6e4aa294df0cd4e49931311503470aebd8
Darren Schroeder <fdncred@gmail.com>
2018-11-09 14:41:06 -0600

    Updated Operations Tests so that their names are unique. VSCode/VStudio was getting confused with the same named tests.

--- test/NumSharp.UnitTest/Operations/Matrix.Substraction.Test.cs
+++ test/NumSharp.UnitTest/Operations/Matrix.Substraction.Test.cs
@@ -6,37 +6,37 @@ using System.Text;
 using NumSharp.Extensions;
 using System.Linq;
 
-namespace NumSharp.UnitTest
+namespace NumSharp.UnitTest.Operations
 {
     [TestClass]
     public class MatrixSubstractionTest
     {
-        
+
         [TestMethod]
-        public void  DoubleTwo2D()
+        public void DoubleTwo2D_MatrixSubstraction()
         {
             var np1 = new Matrix<Double>("1 2 3;4 5 6;7 8 9");
             var np2 = new Matrix<Double>("1 2 3;4 5 6;7 8 9");
 
             var np3 = np1 - np2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new double[]{0,0,0,0,0,0,0,0,0},np3.Data ));
+            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 }, np3.Data));
         }
+
         [TestMethod]
-        public void  ComplexTwo2D()
+        public void ComplexTwo2D_MatrixSubstraction()
         {
             var np1 = new Matrix<Complex>();
             var np2 = new Matrix<Complex>();
 
-            np1.Data = new Complex[] {new Complex(1,2), new Complex(3,4),new Complex(5,6), new Complex(7,8)};
-            np2.Data = new Complex[] {new Complex(8,7), new Complex(6,5),new Complex(4,3), new Complex(2,1)};
-            
+            np1.Data = new Complex[] { new Complex(1, 2), new Complex(3, 4), new Complex(5, 6), new Complex(7, 8) };
+            np2.Data = new Complex[] { new Complex(8, 7), new Complex(6, 5), new Complex(4, 3), new Complex(2, 1) };
+
             var np3 = np1 - np2;
 
-            var expArray = new Complex[] {new Complex(-7,-5), new Complex(-3,-1),new Complex(1,3), new Complex(5,7)};
+            var expArray = new Complex[] { new Complex(-7, -5), new Complex(-3, -1), new Complex(1, 3), new Complex(5, 7) };
 
-            Assert.IsTrue(Enumerable.SequenceEqual(expArray,np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(expArray, np3.Data));
         }
-        
     }
 }
--- test/NumSharp.UnitTest/Operations/NDArray.Multiplication.Test.cs
+++ test/NumSharp.UnitTest/Operations/NDArray.Multiplication.Test.cs
@@ -6,55 +6,57 @@ using System.Text;
 using NumSharp.Extensions;
 using System.Linq;
 
-namespace NumSharp.UnitTest
+namespace NumSharp.UnitTest.Operations
 {
     [TestClass]
     public class NDArrayMultiplicationTest
     {
-        
         [TestMethod]
-        public void  DoubleTwo1D()
+        public void DoubleTwo1D_NDArrayMultiplication()
         {
-            var np1 = new NDArray<double>().ARange(4,1,1).reshape(1,3);
-            var np2 = new NDArray<double>().ARange(5,2,1).reshape(1,3);
+            var np1 = new NDArray<double>().ARange(4, 1, 1).reshape(1, 3);
+            var np2 = new NDArray<double>().ARange(5, 2, 1).reshape(1, 3);
 
             var np3 = np1 * np2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new double[]{2,6,12},np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 2, 6, 12 }, np3.Data));
         }
+
         [TestMethod]
-        public void  ComplexTwo1D()
+        public void ComplexTwo1D_NDArrayMultiplication()
         {
-            var np1 = new NDArray<Complex>(); 
-            np1.Data = new Complex[]{new Complex(1,2),new Complex(3,4)};
-            
-            var np2 = new NDArray<Complex>(); 
-            np2.Data = new Complex[]{new Complex(5,6),new Complex(7,8)};
+            var np1 = new NDArray<Complex>();
+            np1.Data = new Complex[] { new Complex(1, 2), new Complex(3, 4) };
+
+            var np2 = new NDArray<Complex>();
+            np2.Data = new Complex[] { new Complex(5, 6), new Complex(7, 8) };
 
             var np3 = np1 * np2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[]{new Complex(-7,16),new Complex(-11,52)},np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(-7, 16), new Complex(-11, 52) }, np3.Data));
 
         }
+
         [TestMethod]
-        public void Double1DPlusOffset()
+        public void Double1DPlusOffset_NDArrayMultiplication()
         {
             var np1 = new NDArray<double>();
-            np1.Data = new double[]{1,2,3};
+            np1.Data = new double[] { 1, 2, 3 };
 
             var np3 = np1 * 2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new double[]{2,4,6},np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 2, 4, 6 }, np3.Data));
         }
+
         [TestMethod]
-        public void Complex1DPlusOffset()
+        public void Complex1DPlusOffset_NDArrayMultiplication()
         {
-            var np1 = new NDArray<Complex>(); 
-            np1.Data = new Complex[]{new Complex(1,2),new Complex(3,4)};
+            var np1 = new NDArray<Complex>();
+            np1.Data = new Complex[] { new Complex(1, 2), new Complex(3, 4) };
 
-            var np2 = np1 * new Complex(1,2);    
+            var np2 = np1 * new Complex(1, 2);
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[]{new Complex(-3,4),new Complex(-5,10)},np2.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(-3, 4), new Complex(-5, 10) }, np2.Data));
         }
     }
 }
