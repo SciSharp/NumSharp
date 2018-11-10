9cceae17087d5f375001a2943c5af20fb9d40899
dotchris90 <dotchris@online.de>
2018-11-10 20:50:24 +0100

    Feat : Add multi_dot with test

---
+++ src/NumSharp/LinearAlgebra/NdArray.multi_dot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using NumSharp.Extensions;
+
+namespace NumSharp
+{
+    public partial class NDArray<T> 
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="np1"></param>
+        /// <param name="np2"></param>
+        /// <typeparam name="TData"></typeparam>
+        /// <returns></returns>
+        public NDArray<T> multi_dot(params NDArray<T>[] np2Multi)
+        {
+            var np2 = np2Multi.Last(); 
+
+            if ((this.Shape.Length == 1 ) & (np2.Shape.Length == 1))
+                if (this.Shape.Shapes[0] != np2.Shape.Shapes[0])
+                    throw new Exception("The Dot method does not work with this shape or was not already implemented."); 
+                else 
+                {
+                    np2.Shape = new Shape(np2.Data.Length,1);
+                    this.Shape = new Shape(1,this.Data.Length);
+                }
+            else
+                if (this.Shape.Shapes[1] != np2.Shape.Shapes[0])
+                    throw new Exception("The Dot method does not work with this shape or was not already implemented.");
+            
+            var prod = this.dot(np2Multi[0]);
+
+            for(int idx = 1;idx < np2Multi.Length;idx++)
+            {
+                prod = prod.dot(np2Multi[idx]);
+            }
+            
+            return prod;
+        }
+    }
+}
---
+++ test/NumSharp.UnitTest/LinearAlgebra/NdArray.multi_dot.Test.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp.Extensions;
+using System.Linq;
+
+namespace NumSharp.UnitTest.LinearAlgebra
+{
+    [TestClass]
+    public class MultiMatrixDotTest
+    {
+        [TestMethod]
+        public void ThreeMultiplication()
+        {
+            NDArray<double> np1 = new NDArray<double>().ARange(4).reshape(2,2);
+            NDArray<double> np2 = np1.inv();
+            NDArray<double> np3 = new NDArray<double>().Onces(2,2);
+
+            var OncesMatrix = np1.multi_dot(np2,np3);
+
+            Assert.IsTrue(OncesMatrix[0,0] == 1);
+            Assert.IsTrue(OncesMatrix[1,0] == 1);
+            Assert.IsTrue(OncesMatrix[0,1] == 1);
+            Assert.IsTrue(OncesMatrix[1,1] == 1);
+
+        }
+    }
+}
