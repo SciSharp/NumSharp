55a487cb896542b2844275116ffe6b24a4523bbc
Esther2013 <estherhu2012@gmail.com>
2018-11-13 22:13:11 -0600

    updated README

--- test/NumSharp.UnitTest/NdArrayRandom.Test.cs
+++ test/NumSharp.UnitTest/NdArrayRandom.Test.cs
@@ -10,12 +10,14 @@ namespace NumSharp.UnitTest
     public class NDArrayRandomTest
     {
         NumPy<double> np = new NumPy<double>();
+
         [TestMethod]
         public void randn()
         {
             var n = np.random.randn(5, 2);
         }
 
+        [TestMethod]
         public void normal()
         {
             var n = np.random.normal(0, 1, 5);
