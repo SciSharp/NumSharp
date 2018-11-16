5cc91fdcb15a4df9324095895c65cf3eb6832059
dotchris90 <dotchris@online.de>
2018-11-16 19:55:44 +0100

    Add Transpose

---
+++ src/NumSharp/LinearAlgebra/NdArray.Transpose.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using NumSharp.Extensions;
+
+namespace NumSharp
+{
+    public partial class NDArray<T> 
+    {
+        public NDArray<T> transpose()
+        {
+            var np = new NDArray<T>();
+            np.Data = this.Data;
+
+            if (NDim == 1)
+            {
+                np.Shape = new Shape(1,Shape.Shapes[0]);
+            }
+            else 
+            {
+                np.Shape = new Shape(this.Shape.Shapes.Reverse().ToArray());
+            }
+            
+            return np;
+        }
+    }
+}
