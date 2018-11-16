f1c3ec6753e42336006fd0d68816ce662759d4a7
dotchris90 <dotchris@online.de>
2018-11-16 19:56:31 +0100

    First  tests for doc demos

---
+++ test/NumSharp.UnitTest/Docs/NDArray.LinAlgebra.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp.Extensions;
+
+namespace NumSharp.UnitTest.Docs
+{
+    [TestClass]
+    public class LinearAlgebraTester
+    {
+        [TestMethod]
+        public void LinearRegression()
+        {
+            var np = new NumPy<double>();
+
+            // the time array 
+            var time = np.linspace(0,10,1000);
+            
+            // the values over t - linear dependence with noice 
+            var values = time * 3 + 5 + np.random.randn(time.Data.Length);
+
+            var matrixA = new NDArray<double>().array(new double[2000]);
+         
+        }
+    }
+}
