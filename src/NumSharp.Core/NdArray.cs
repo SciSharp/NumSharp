527284c04941905e8c5a2edc868c0706fdb3a27c
dotchris90 <dotchris@online.de>
2018-11-22 18:48:06 +0100

    feat : Add Base constructor

--- src/NumSharp.Core/NdArray.cs
+++ src/NumSharp.Core/NdArray.cs
@@ -54,7 +54,10 @@ namespace NumSharp.Core
         /// Total of elements
         /// </summary>
         public int Size => Shape.Size;
+        public NDArray()
+        {
 
+        }
         public NDArray(Type dtype)
         {
             this.dtype = dtype;
