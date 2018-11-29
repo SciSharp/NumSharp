6e875b70f9b0473dd78110dc92ace5e53b3b8457
dotchris90 <dotchris@online.de>
2018-11-29 12:43:20 +0100

    Feat : Create generic NDArrays by non generic.

---
+++ src/NumSharp.Core/Creation/NdArray.MakeGeneric.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace NumSharp.Core
+{
+    public partial class NDArray
+    {
+        public NumSharp.Generic.NDArray<T> MakeGeneric<T>() where T : struct
+        {
+            var genericArray = new NumSharp.Generic.NDArray<T>(this.Storage.Shape);
+
+            genericArray.Storage.SetData(this.Storage.GetData<T>());
+
+            return genericArray;
+        }
+    }
+}
---
+++ test/NumSharp.UnitTest/Creation/NdArray.MakeGeneric.Test.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp.Core.Extensions;
+using System.Linq;
+using NumSharp.Core;
+
+namespace NumSharp.UnitTest.Extensions
+{
+    [TestClass]
+    public class NdArrayMakeGenericTester
+    {
+        NumPy np = new NumPy();
+
+        [TestMethod]
+        public void Array1DimGeneric()
+        {
+            var list = new double[] { 1.1, 2.2, 3.3 };
+            var arrayDouble = np.array(list).MakeGeneric<double>();
+            var arrayInt = np.array(list).MakeGeneric<int>();
+
+            Assert.IsTrue(arrayDouble[0] == 1.1);
+            Assert.IsTrue(arrayDouble[1] == 2.2);
+            Assert.IsTrue(arrayDouble[2] == 3.3);
+
+            Assert.IsTrue(arrayInt[0] == 1);
+            Assert.IsTrue(arrayInt[1] == 2);
+            Assert.IsTrue(arrayInt[2] == 3);
+            
+        }    
+    }
+}
