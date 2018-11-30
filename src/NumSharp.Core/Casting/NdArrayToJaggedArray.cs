86959996066c149d312b913e4c6282e40aab8fa9
dotchris90 <dotchris@online.de>
2018-11-30 09:32:48 +0100

    Feat : Add casting method for .NET arrays

---
+++ src/NumSharp.Core/Casting/NdArrayToJaggedArray.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * NumSharp
+ * Copyright (C) 2018 Haiping Chen
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache License 2.0 as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the Apache License 2.0
+ * along with this program.  If not, see <http://www.apache.org/licenses/LICENSE-2.0/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Collections;
+using NumSharp.Core;
+
+namespace NumSharp.Core
+{
+    public partial class NDArray
+    {
+        public Array ToJaggedArray<T>()
+        {
+            Array dotNetArray = null;
+
+            switch (NDim)
+            {
+                case 1 : 
+                {
+                    dotNetArray = Storage.GetData<T>() ;    
+                    break;
+                }
+                case 2 : 
+                {
+                    T[] data = Storage.GetData<T>();
+                    T[][] dotNetArrayPuffer = new T[Shape.Shapes[0]][]; 
+                    for (int idx = 0; idx < dotNetArrayPuffer.Length;idx++)
+                        dotNetArrayPuffer[idx] = new T[Shape.Shapes[1]];
+                        
+                    for (int idx = 0;idx < data.Length;idx++ )
+                        dotNetArrayPuffer[idx/Shape.Shapes[1]][idx%Shape.Shapes[1]] = data[idx];
+                    
+                    dotNetArray = dotNetArrayPuffer;
+                    
+                    break;
+                }
+
+            }
+
+            return dotNetArray;
+        }
+        
+    }
+}
---
+++ test/NumSharp.UnitTest/Casting/NDArray.ToDotNetArray.Test.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NumSharp.Core.Extensions;
+using NumSharp.Core;
+
+namespace NumSharp.UnitTest
+{
+    [TestClass]
+    public class CastingTester
+    {
+        NumPy np = new NumPy();
+        [TestMethod]
+        public void ToDotNetArray()
+        {
+            var oneDArray = np.arange(10.0);
+            var oneDArrayDotNet = (double[]) oneDArray.ToMuliDimArray<double>();
+
+            var twoDArray = np.arange(8.0).reshape(2,4);
+            var twoDArrayDotNet = (double[,]) twoDArray.ToMuliDimArray<double>();
+
+        }
+    }
+}
