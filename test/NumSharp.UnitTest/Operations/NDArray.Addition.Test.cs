7d0228c3f9df7c2260882ebb939183cf7d1e9778
dotchris90 <dotchris@online.de>
2018-12-01 20:37:14 +0100

    Fix : Operations for new ndarray

--- test/NumSharp.UnitTest/Operations/NDArray.Addition.Test.cs
+++ test/NumSharp.UnitTest/Operations/NDArray.Addition.Test.cs
@@ -15,12 +15,12 @@ namespace NumSharp.UnitTest.Operations
         [TestMethod]
         public void DoubleTwo1D_NDArrayAddition()
         {
-            var np1 = new NDArrayGeneric<double>().arange(4, 1);
-            var np2 = new NDArrayGeneric<double>().arange(5, 2);
+            var np1 = new NDArray(typeof(int)).arange(4, 1);
+            var np2 = new NDArray(typeof(int)).arange(5, 2);
 
             var np3 = np1 + np2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 3, 5, 7 }, np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new int[] { 3, 5, 7 }, np3.Storage.GetData<int>()));
         }
 
         [TestMethod]
@@ -29,78 +29,74 @@ namespace NumSharp.UnitTest.Operations
             var data1 = new Complex[] { new Complex(1, 2), new Complex(3, 4) };
             var data2 = new Complex[] { new Complex(5, 6), new Complex(7, 8) };
 
-            var np1 = new NDArrayGeneric<Complex>();
-            np1.Shape = new Shape(new int[] { 2 });
-            np1.Data = data1;
+            var np1 = new NDArray(typeof(Complex),2);
+            
+            np1.Storage.SetData(data1);
 
-            var np2 = new NDArrayGeneric<Complex>();
-            np2.Shape = new Shape(new int[] { 2 });
-            np2.Data = data2;
+            var np2 = new NDArray(typeof(Complex),2);
+            
+            np2.Storage.SetData(data2);
 
             var np3 = np1 + np2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(6, 8), new Complex(10, 12) }, np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(6, 8), new Complex(10, 12) }, np3.Storage.GetData<Complex>()));
 
         }
 
         [TestMethod]
         public void Double1DPlusOffset_NDArrayAddition()
         {
-            var np1 = new NDArrayGeneric<double>().arange(4, 1);
+            var np1 = new NDArray(typeof(int)).arange(4, 1);
 
             var np3 = np1 + 2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 3, 4, 5 }, np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 3, 4, 5 }, np3.Storage.GetData<double>()));
         }
 
         [TestMethod]
         public void Complex1DPlusOffset_NDArrayAddition()
         {
-            var np1 = new NDArrayGeneric<Complex>();
-            np1.Data = new Complex[] { new Complex(1, 2), new Complex(3, 4) };
-            np1.Shape = new Shape(new int[] { 2 });
-
+            var np1 = new NDArray(typeof(Complex),2);
+            np1.Storage.SetData(new Complex[] { new Complex(1, 2), new Complex(3, 4) });
+            
             var np2 = np1 + new Complex(1, 2);
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(2, 4), new Complex(4, 6) }, np2.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(2, 4), new Complex(4, 6) }, np2.Storage.GetData<Complex>()));
         }
 
         [TestMethod]
         public void Complex2DArray_NDArrayAddition()
         {
-            var np1 = new NDArrayGeneric<Complex>();
-            np1.Data = new Complex[] { new Complex(4, 1), new Complex(3, 5), new Complex(0, -2), new Complex(-3, 2) };
-            np1.Shape = new Shape(new int[] { 2, 2 });
+            var np1 = new NDArray(typeof(Complex),2,2);
+            np1.Storage.SetData(new Complex[] { new Complex(4, 1), new Complex(3, 5), new Complex(0, -2), new Complex(-3, 2) });
+            
 
-            var np2 = new NDArrayGeneric<Complex>();
-            np2.Data = new Complex[] { new Complex(1, 2), new Complex(3, 4), new Complex(1, 2), new Complex(3, 4) };
-            np2.Shape = new Shape(new int[] { 2, 2 });
-
+            var np2 = new NDArray(typeof(Complex),2,2);
+            np2.Storage.SetData(new Complex[] { new Complex(1, 2), new Complex(3, 4), new Complex(1, 2), new Complex(3, 4) });
+            
             var np3 = np1 + np2;
 
             // expected
             var np4 = new Complex[] { new Complex(5, 3), new Complex(6, 9), new Complex(1, 0), new Complex(0, 6) };
 
-            Assert.IsTrue(Enumerable.SequenceEqual(np3.Data, np4));
+            Assert.IsTrue(Enumerable.SequenceEqual(np3.Storage.GetData<Complex>(), np4));
         }
 
         [TestMethod]
         public void Double2DArray_NDArrayAddition()
         {
-            var np1 = new NDArrayGeneric<double>();
-            np1.Data = new double[] { 1, 2, 3, 4, 5, 6 };
-            np1.Shape = new Shape(new int[] { 2, 3 });
-
-            var np2 = new NDArrayGeneric<double>();
-            np2.Data = new double[] { 9, 8, 7, 6, 5, 4 };
-            np2.Shape = new Shape(new int[] { 2, 3 });
-
+            var np1 = new NDArray(typeof(double),2,3);
+            np1.Storage.SetData(new double[] { 1, 2, 3, 4, 5, 6 });
+            
+            var np2 = new NDArray(typeof(double),2,3);
+            np2.Storage.SetData(new double[] { 9, 8, 7, 6, 5, 4 });
+            
             var np3 = np1 + np2;
 
             // expected
             var np4 = new double[] { 10, 10, 10, 10, 10, 10 };
 
-            Assert.IsTrue(Enumerable.SequenceEqual(np3.Data, np4));
+            Assert.IsTrue(Enumerable.SequenceEqual(np3.Storage.GetData<double>(), np4));
         }
     }
 }
--- test/NumSharp.UnitTest/Operations/NDArray.Multiplication.Test.cs
+++ test/NumSharp.UnitTest/Operations/NDArray.Multiplication.Test.cs
@@ -14,49 +14,49 @@ namespace NumSharp.UnitTest.Operations
         [TestMethod]
         public void DoubleTwo1D_NDArrayMultiplication()
         {
-            var np1 = new NDArrayGeneric<double>().arange(4, 1, 1).reshape(1, 3);
-            var np2 = new NDArrayGeneric<double>().arange(5, 2, 1).reshape(1, 3);
+            var np1 = new NDArray(typeof(double)).arange(4, 1, 1).reshape(1, 3);
+            var np2 = new NDArray(typeof(double)).arange(5, 2, 1).reshape(1, 3);
 
             var np3 = np1 * np2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 2, 6, 12 }, np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 2, 6, 12 }, np3.Storage.GetData<double>()));
         }
 
         [TestMethod]
         public void ComplexTwo1D_NDArrayMultiplication()
         {
-            var np1 = new NDArrayGeneric<Complex>();
-            np1.Data = new Complex[] { new Complex(1, 2), new Complex(3, 4) };
+            var np1 = new NDArray(typeof(Complex),2);
+            np1.Storage.SetData(new Complex[] { new Complex(1, 2), new Complex(3, 4) });
 
-            var np2 = new NDArrayGeneric<Complex>();
-            np2.Data = new Complex[] { new Complex(5, 6), new Complex(7, 8) };
+            var np2 = new NDArray(typeof(Complex),2);
+            np2.Storage.SetData(new Complex[] { new Complex(5, 6), new Complex(7, 8) });
 
             var np3 = np1 * np2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(-7, 16), new Complex(-11, 52) }, np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(-7, 16), new Complex(-11, 52) }, np3.Storage.GetData<Complex>()));
 
         }
 
         [TestMethod]
         public void Double1DPlusOffset_NDArrayMultiplication()
         {
-            var np1 = new NDArrayGeneric<double>();
-            np1.Data = new double[] { 1, 2, 3 };
+            var np1 = new NDArray(typeof(double),3);
+            np1.Storage.SetData(new double[] { 1, 2, 3 });
 
             var np3 = np1 * 2;
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 2, 4, 6 }, np3.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new double[] { 2, 4, 6 }, np3.Storage.GetData<double>()));
         }
 
         [TestMethod]
         public void Complex1DPlusOffset_NDArrayMultiplication()
         {
-            var np1 = new NDArrayGeneric<Complex>();
-            np1.Data = new Complex[] { new Complex(1, 2), new Complex(3, 4) };
+            var np1 = new NDArray(typeof(Complex),2);
+            np1.Storage.SetData(new Complex[] { new Complex(1, 2), new Complex(3, 4) });
 
             var np2 = np1 * new Complex(1, 2);
 
-            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(-3, 4), new Complex(-5, 10) }, np2.Data));
+            Assert.IsTrue(Enumerable.SequenceEqual(new Complex[] { new Complex(-3, 4), new Complex(-5, 10) }, np2.Storage.GetData<Complex>()));
         }
     }
 }
