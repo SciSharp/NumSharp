bd41f1edf797bc7cf6cb18d8f212c6cd6d693bc0
Oceania2018 <haiping008@gmail.com>
2018-12-03 11:26:38 -0600

    numpy.asmatrix

---
+++ src/NumSharp.Core/Creation/NumPy.asmatrix.cs
@@ -0,0 +1,15 @@
+﻿using NumSharp.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumSharp.Core
+{
+    public partial class NumPy
+    {
+        public Matrix asmatrix(NDArray nd)
+        {
+            return nd.AsMatrix();
+        }
+    }
+}
--- test/NumSharp.UnitTest/LinearAlgebra/NdArray.Transpose.cs
+++ test/NumSharp.UnitTest/LinearAlgebra/NdArray.Transpose.cs
@@ -10,21 +10,21 @@ using NumSharp.Core;
 namespace NumSharp.UnitTest.LinearAlgebra
 {
     [TestClass]
-    public class TransposeTest
+    public class TransposeTest : TestBase
     {
         [TestMethod]
         public void TwoxThree()
         {
-            NDArrayGeneric<double> np1 = new NumPyGeneric<double>().arange(6).reshape(3,2);
+            NDArray np1 = np.arange(6).reshape(3,2);
             
             var np1Transposed = np1.transpose();
 
-            Assert.IsTrue(np1Transposed[0,0] == 0);
-            Assert.IsTrue(np1Transposed[0,1] == 2);
-            Assert.IsTrue(np1Transposed[0,2] == 4);
-            Assert.IsTrue(np1Transposed[1,0] == 1);
-            Assert.IsTrue(np1Transposed[1,1] == 3);
-            Assert.IsTrue(np1Transposed[1,2] == 5);
+            Assert.AreEqual(np1Transposed[0,0], 0);
+            Assert.AreEqual(np1Transposed[0,1], 2);
+            Assert.AreEqual(np1Transposed[0,2], 4);
+            Assert.AreEqual(np1Transposed[1,0], 1);
+            Assert.AreEqual(np1Transposed[1,1], 3);
+            Assert.AreEqual(np1Transposed[1,2], 5);
 
         }
     }
