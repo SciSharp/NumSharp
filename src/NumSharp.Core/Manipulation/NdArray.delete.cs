80213eee6d1363cddff2feeab50002ebe7547385
Oceania2018 <haiping008@gmail.com>
2018-12-03 23:34:19 -0600

    Extend to ndarray.ravel

--- src/NumSharp.Core/Manipulation/NdArray.delete.cs
+++ src/NumSharp.Core/Manipulation/NdArray.delete.cs
@@ -7,47 +7,10 @@ namespace NumSharp.Core
 {
     public partial class NDArray
     {
-        public NDArray delete(IEnumerable delete)
+        public NDArray ravel()
         {
-            var np = new NumPy();
-
-            var sysArr = this.Storage.GetData();
-
-            NDArray res = null;
-
-            switch( sysArr)
-            {
-                case double[] castedSysArr : 
-                {
-                    var castedDelete = delete as double[];
-
-                    res = np.array(castedSysArr.Where(x => !castedDelete.Contains(x) ).ToArray());
-
-                    break;
-                }
-                case float[] castedSysArr : 
-                {
-                    var castedDelete = delete as float[];
-
-                    res = np.array(castedSysArr.Where(x => !castedDelete.Contains(x) ).ToArray());
-
-                    break;
-                }
-                case int[] castedSysArr :
-                {
-                    var castedDelete = delete as int[];
-
-                    res = np.array(castedSysArr.Where(x => !castedDelete.Contains(x) ).ToArray());
-
-                    break;
-                }
-                default : 
-                {
-                    throw new IncorrectTypeException();
-                }
-            }
-            
-            return res;
+            Storage.Shape = new Shape(shape.Size);
+            return this;
         }
     }
 }
