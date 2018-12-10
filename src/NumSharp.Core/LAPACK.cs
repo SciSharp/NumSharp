8ae010a66b2876b5bd2f34102074505d102c1e37
dotchris90 <dotchris@online.de>
2018-12-10 16:28:17 +0100

    Add LAPACK first tries to Core

---
+++ src/NumSharp.Core/LAPACK.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NumSharp.Core
+{
+    public static class LAPACK
+    {
+        [DllImport("lapack")]
+        public static extern void dgesv_(ref int n, ref int nrhs, double[] a, ref int lda, int[] ipiv, double[] b, ref int ldb, ref int info  );
+        [DllImport("lapack")]
+        public static extern void dgeqrf_(ref int m, ref int n, double[] a, ref int lda, double[] tau, double[] work, ref int lwork, ref int info);
+        
+    }
+}
