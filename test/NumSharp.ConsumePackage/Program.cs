ec56c69746f8bc2e4d4b0478797996062ebb2507
dotchris90 <dotchris@online.de>
2018-12-12 19:35:23 +0100

    Feat : Add project for package consuming

---
+++ test/NumSharp.ConsumePackage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumSharp.ConsumePackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var np = new NumSharp.Core.NumPy();
+
+            var A = np.array(new double[]{1,2,3,4}).reshape(2,2);
+
+            var b = np.array(new double[] { 1,2}).reshape(2,1);
+
+            var c = A.lstqr(b);
+
+            Console.WriteLine("If there is no error --> package was sucessfully consumed! :)");
+        }
+    }
+}
