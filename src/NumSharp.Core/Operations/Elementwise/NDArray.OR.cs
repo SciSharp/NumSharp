666617e13984714cb01aeb02c3cda1d83c3505e6
dotchris90 <dotchris@online.de>
2018-12-25 21:19:05 +0100

    Feat : Add Numsharp OR operator

---
+++ src/NumSharp.Core/Operations/Elementwise/NDArray.OR.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace NumSharp.Core
+{
+    public partial class NDArray
+    {
+        public static NumSharp.Generic.NDArray<bool> operator |(NDArray np_, NDArray obj_)
+        {
+            var boolTensor = new NDArray(typeof(bool),np_.shape);
+            bool[] bools = boolTensor.Storage.GetData() as bool[];
+
+            bool[] np = np_.MakeGeneric<bool>().Storage.GetData() as bool[];
+            bool[] obj = obj_.MakeGeneric<bool>().Storage.GetData() as bool[];
+
+             for(int idx = 0;idx < bools.Length;idx++)
+                bools[idx] = np[idx] || obj[idx];
+            
+            return boolTensor.MakeGeneric<bool>();
+        }
+    }
+}
