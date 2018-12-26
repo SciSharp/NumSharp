b557da430ce9b2681c891436fef2701a25b6a572
dotchris90 <dotchris@online.de>
2018-12-26 17:05:51 +0100

    Feat : Add np.mgrid API

---
+++ src/NumSharp.Core/Creation/np.mgrid.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+ 
+namespace NumSharp.Core
+{
+	public static partial class np
+	{
+        public static (NDArray,NDArray) mgrid(NDArray nd1, NDArray nd2)
+        {
+            return nd1.mgrid(nd2);
+        }
+    }
+}
