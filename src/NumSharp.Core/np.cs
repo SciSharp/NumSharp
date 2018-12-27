dee732a8196f88d8ecf0078e926625598c6e041d
Oceania2018 <haiping008@gmail.com>
2018-12-27 11:54:15 -0600

    released patch v0.6.2

--- src/NumSharp.Core/np.cs
+++ src/NumSharp.Core/np.cs
@@ -11,9 +11,9 @@ namespace NumSharp.Core
     {
         // https://docs.scipy.org/doc/numpy-1.15.0/user/basics.types.html
         public static Type int16 => typeof(short);
-        public static Type int32 => typeof(int);
-        public static Type int64 => typeof(Int64);
-        public static Type float32 => typeof(float);
+        public static Type int32 => typeof(int); // Int32
+        public static Type int64 => typeof(long); // Int64
+        public static Type float32 => typeof(float); // Single
         public static Type float64 => typeof(double);
         public static Type chars => typeof(string);
         public static NumPyRandom random => new NumPyRandom();
