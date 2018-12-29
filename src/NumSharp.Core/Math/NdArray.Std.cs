4954f672e616cabc8b05d426f309c4c6486aef90
dotchris90 <dotchris@online.de>
2018-12-29 19:43:39 +0100

    Feat : Add Std method from numpy api

---
+++ src/NumSharp.Core/Math/NdArray.Std.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NumSharp.Core.Extensions;
+
+namespace NumSharp.Core
+{
+    public partial class NDArray
+    {
+        public NDArray std(int axis = -1, Type dtype = null)
+        {
+            dtype = (dtype == null) ? typeof(double) : dtype;
+
+            // in case have 1D array but user still using axis 0 ... can be used like -1
+            axis = (axis == 0 && this.ndim == 1) ? -1 : axis;
+
+            Array data = this.Storage.GetData();
+
+            NDArray stdArr = new NDArray(dtype);
+
+            if (axis == -1)
+            {
+                double mean = this.mean(axis).MakeGeneric<double>()[0];
+                double sum = 0;
+                for(int idx = 0; idx < data.Length;idx++)                
+                    sum += Math.Pow(Convert.ToDouble(data.GetValue(idx)) - mean,2);
+                
+                double stdValue = Math.Sqrt(sum / this.size);
+                stdArr.Storage.Allocate(dtype,new Shape(1),1);
+                var puffer = Array.CreateInstance(dtype,1);
+                puffer.SetValue(stdValue,0);
+                stdArr.Storage.SetData(puffer);
+            }
+            else 
+            {
+                double[] stdValue = null;
+                if (axis == 0)
+                {
+                    double[] sum = new double[this.shape[1]];
+                    stdValue = new double[sum.Length];
+
+                    double[] mean = this.mean(axis).Storage.GetData<double>();
+
+                    for (int idx = 0; idx < sum.Length;idx++)
+                    {
+                        for(int jdx =0; jdx < this.shape[0];jdx++)
+                        {
+                            sum[idx] += Math.Pow(Convert.ToDouble(this[jdx,idx]) - mean[idx],2); 
+                        }
+                        stdValue[idx] = Math.Sqrt(sum[idx] / this.shape[0]);
+                    }
+                            
+                }
+                else if (axis == 1)
+                {
+                    double[] sum = new double[this.shape[0]];
+                    stdValue = new double[sum.Length];
+
+                    double[] mean = this.mean(axis).Storage.GetData<double>();
+
+                    for (int idx = 0; idx < sum.Length;idx++)
+                    {
+                        for(int jdx =0; jdx < this.shape[1];jdx++)
+                        {
+                            sum[idx] += Math.Pow(Convert.ToDouble(this[idx,jdx]) - mean[idx],2); 
+                        }
+                        stdValue[idx] = Math.Sqrt(sum[idx] / this.shape[1]);
+                    }
+                }
+                else
+                {
+                    throw new NotImplementedException();
+                }
+                stdArr.Storage.Allocate(dtype,new Shape(stdValue.Length),1);
+                stdArr.Storage.SetData(stdValue);
+            }
+            return stdArr;
+        }
+    }
+}
--- test/NumSharp.UnitTest/Extensions/NDArray.Std.Test.cs
+++ test/NumSharp.UnitTest/Extensions/NDArray.Std.Test.cs
@@ -13,11 +13,11 @@ namespace NumSharp.UnitTest.Extensions
         [TestMethod]
         public void StdTest()
         {
-            var np = new NDArray(typeof(double)).arange(4).reshape(2,2).MakeGeneric<double>();
+            var nd1 = new NDArray(typeof(double)).arange(4).reshape(2,2).MakeGeneric<double>();
 
-            //Assert.IsTrue(Enumerable.SequenceEqual(np.s .Data, new double[] { 1.1180339887498949 }));
-            // Assert.IsTrue(Enumerable.SequenceEqual(np.Std(0).Data, new double[] { 1, 1 }));
-            // Assert.IsTrue(Enumerable.SequenceEqual(np.Std(1).Data, new double[] { 0.5, 3.5 }));
+            Assert.IsTrue(Enumerable.SequenceEqual(nd1.std().Data<double>(), new double[] { 1.1180339887498949 }));
+            Assert.IsTrue(Enumerable.SequenceEqual(nd1.std(0).Data<double>(), new double[] { 1, 1 }));
+            Assert.IsTrue(Enumerable.SequenceEqual(nd1.std(1).Data<double>(), new double[] { 0.5, 0.5 }));
         }
     }
 }
