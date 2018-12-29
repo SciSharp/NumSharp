bfeb8e30ff66651e78cb0904942a5cfee60e7a09
dotchris90 <dotchris@online.de>
2018-12-29 13:57:33 +0100

    Fix : remove items if they exist (not always try)

--- ExecuteUnitTests.ps1
+++ ExecuteUnitTests.ps1
@@ -4,12 +4,31 @@ Write-Output "--------------------"
 Write-Output "Start with building"
 Write-Output "--------------------"
 
-Remove-Item -Path .\test\NumSharp.UnitTest\bin -Recurse
+$projectFolders = @{};
+
+$projectFolders['projectRoot'] = $PSScriptRoot;
+$projectFolders['UnitTest'] = Join-Path $projectFolders['projectRoot'] test/NumSharp.UnitTest;
+$projectFolders['UnitTest.Bin'] = Join-Path $projectFolders['UnitTest'] bin
+$projectFolders['UnitTest.CSPROJ'] = Join-Path $projectFolders['UnitTest'] NumSharp.UnitTest.csproj
+
+# clear all items before testing
+if (Test-Path -Path ($projectFolders['UnitTest.Bin'] ) )
+{
+    Write-Output "bin folder found - remove items";
+    $puffer = Get-ChildItem -Path $projectFolders['UnitTest.Bin'] -Recurse;
+
+    for($idx = 0;$idx -lt $puffer.Length;$idx++)
+    {
+        Write-Output ("    - " + $puffer[$idx] );
+    }
+
+    Remove-Item -Path $projectFolders['UnitTest.Bin'] -Recurse
+}
 
 dotnet build ./src/NumSharp.Core/NumSharp.Core.csproj
 dotnet build ./test/NumSharp.UnitTest/NumSharp.UnitTest.csproj
 
-$generatedDlls = Get-ChildItem -Recurse -File -Path .\test\NumSharp.UnitTest\bin
+$generatedDlls = Get-ChildItem -Recurse -File -Path $projectFolders['UnitTest.Bin']
 
 Write-Output "following items exist:"
 
@@ -18,4 +37,4 @@ for($idx =0; $idx -lt $generatedDlls.Length;$idx++)
     Write-Output ("   - " + $generatedDlls[$idx].Name)
 } 
 
-dotnet test ./test/NumSharp.UnitTest/NumSharp.UnitTest.csproj
+dotnet test $projectFolders['UnitTest.CSPROJ']
