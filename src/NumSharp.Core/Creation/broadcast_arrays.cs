149bc957b7a20ebffabc2be5c2d52989c9203165
Bo Peng <bolopeng1991@gmail.com>
2019-03-11 17:17:12 -0500

    implemented 1)np.meshgrid 2) np.broadcast_array 3) _broadcast_to 4) _broadcast_shape

---
+++ src/NumSharp.Core/Creation/broadcast_arrays.cs
@@ -0,0 +1,56 @@
+﻿using NumSharp.Core.Casting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumSharp.Core
+{
+    public static partial class np
+    {
+        public static NDArray[] broadcast_arrays(NDArray nd1, NDArray nd2, bool subok = false)
+        {
+            NDArray args1 = np.array(nd1);
+            NDArray args2 = np.array(nd2);
+            var shape = _broadcast_shape(args1, args2);
+
+            if (nd1.shape == shape && nd2.shape == shape)
+            {
+                return new NDArray[] { nd1, nd2 };
+            }
+
+            return new NDArray[] { _broadcast_to(nd1, shape, subok, false) };
+        }
+
+        private static Shape _broadcast_shape(NDArray nd1, NDArray nd2)
+        {
+            Broadcast b = np.broadcast(nd1, nd2);
+            return b.shape;
+        }
+
+        private static NDArray _broadcast_to(NDArray nd, Shape shape, bool subok, bool rdonly)
+        {
+            double[,] table = new double[shape.Dimensions[0], shape.Dimensions[1]];
+            if (nd.shape[0] == 1) 
+            {// (1,2,3)
+                for (int i = 0; i < shape.Dimensions[0]; i++)
+                {
+                    for (int j = 0; j < shape.Dimensions[1]; j++)
+                    {
+                        table[i, j] = (double)nd.Storage.GetData(0, j); 
+                    }
+                }
+            }
+            else if (nd.shape[1] == 1)
+            {
+                for (int i = 0; i < shape.Dimensions[0]; i++)
+                {
+                    for (int j = 0; j < shape.Dimensions[1]; j++)
+                    {
+                        table[i, j] = (double)nd.Storage.GetData(i, 0);
+                    }
+                }
+            }
+            return new NDArray(table);
+        }
+    }
+}
--- src/NumSharp.Core/Creation/np.meshgrid.cs
+++ src/NumSharp.Core/Creation/np.meshgrid.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NumSharp.Core.Creation
+namespace NumSharp.Core
 {
     static partial class np
     {
@@ -14,12 +14,68 @@ namespace NumSharp.Core.Creation
         /// .. versionchanged:: 1.9
         /// 1-D and 0-D cases are allowed.
         /// </summary>
-        /// <param name="x1"></param>
-        /// <param name="x2"></param>
+        /// <param name="x1"> 1-D arrays representing the coordinates of a grid</param>
+        /// /// <param name="x2"> 1-D arrays representing the coordinates of a grid</param>
         /// <returns></returns>
-        public static NDArray meshgrid(NDArray x1, NDArray x2)
+        public static (NDArray, NDArray) meshgrid(NDArray x1, NDArray x2, Kwargs kwargs =null)
         {
-            throw new NotFiniteNumberException();
+            if (kwargs == null)
+            {
+                kwargs = new Kwargs();
+            }
+            int ndim = 2;
+            var s0 = (1,1);
+            var output = new NDArray[]{ np.asanyarray(x1).reshape(-1,1), np.asanyarray(x2).reshape(1,-1)};
+
+            if (kwargs.indexing == "xy" && ndim > 1)
+            {
+                // Switch first and second axis
+                output[0].reshape(1,-1);
+                output[1].reshape(-1, 1);
+            }
+
+            if (!kwargs.sparse)
+            {
+                // Return the full N-D matrix(not only the 1 - D vector)
+                output = np.broadcast_arrays(output[0], output[1], true);
+            }
+
+            if (kwargs.copy)
+            {
+
+            }
+
+            return (output[0], output[1]);
+        }
+
+    }
+
+    public class Kwargs
+    {
+        public string indexing { get; set; }
+        public bool sparse { get; set; }
+        public bool copy { get; set; }
+        /// <summary>
+        /// Kwargs constructor
+        /// </summary>
+        /// <param name="indexing"> {'xy', 'ij'}, optional Cartesian('xy', default) or matrix('ij') indexing of output.</param>
+        /// <param name="sparse">If True a sparse grid is returned in order to conserve memory. Default is False.</param>
+        /// <param name="copy">If False, a view into the original arrays are returned in order to conserve memory.
+        /// Default is True.Please note that sparse= False, copy= False`` will likely return non-contiguous arrays.  
+        /// Furthermore, more than one element of a broadcast array may refer to a single memory location.
+        /// If you need to write to the arrays, make copies first.</param>
+        public Kwargs(string indexing, bool sparse, bool copy)
+        {
+            this.indexing = indexing;
+            this.sparse = sparse;
+            this.copy = copy;
+        }
+
+        public Kwargs()
+        {
+            this.indexing = "xy";
+            this.sparse = false;
+            this.copy = true;
         }
     }
 }
