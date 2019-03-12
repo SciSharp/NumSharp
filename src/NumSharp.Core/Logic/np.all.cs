69f01b3e3f1efe77a2bf87b65041c603a18d6aef
Oceania2018 <haiping008@gmail.com>
2019-03-11 21:12:26 -0500

    Release v0.8.0

---
+++ src/NumSharp.Core/Logic/np.all.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumSharp.Core
+{
+    public static partial class np
+    {
+        public static NDArray all(NDArray nd, int axis = -1)
+        {
+            NDArray result = null;
+            if(axis == -1)
+            {
+                switch (nd.dtype.Name)
+                {
+                    case "Int32":
+                        {
+                        }
+                        break;
+                    case "Int64":
+                        {
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                throw new NotImplementedException($"np.prod axis {axis}");
+            }
+
+            return result;
+        }
+    }
+}
