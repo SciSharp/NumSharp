7a7c22ff0a031eb2eb4d2a934c8371ea5257c133
dania-vera <dania.tamayo@gmail.com>
2019-03-20 18:16:58 -0300

    Added methods that support to read and write a npy and npz file and a tester.

---
+++ test/NumSharp.UnitTest/np.save_load.Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumSharp.UnitTest.Creation;
+using NumSharp.Core;
+
+namespace NumSharp.UnitTest
+{
+  [TestClass]
+  public class NumpySaveLoad
+  {
+    [TestMethod]
+    public void Run()
+    {
+      int[] x = { 1, 2, 3, 4, 5 };
+      np.Save(x, @"test.npy");
+      np.Save_Npz(x, @"test1.npz");
+      np.Load<int[]>(@"test.npy");
+      np.Load_Npz<int[]>(@"test1.npz");
+    }
+  }
+}
