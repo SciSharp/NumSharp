d4c74b127d05100231c32287799877df566f02be
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-04-05 08:40:32 +0200

    added np.all (without axis)

--- src/NumSharp.Core/Logic/np.all.cs
+++ src/NumSharp.Core/Logic/np.all.cs
@@ -1,34 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
+using NumSharp.Generic;
 
 namespace NumSharp
 {
     public static partial class np
     {
-        public static NDArray all(NDArray nd, int axis = -1)
-        {
-            NDArray result = null;
-            if(axis == -1)
-            {
-                switch (nd.dtype.Name)
-                {
-                    case "Int32":
-                        {
-                        }
-                        break;
-                    case "Int64":
-                        {
-                        }
-                        break;
-                }
-            }
-            else
-            {
-                throw new NotImplementedException($"np.prod axis {axis}");
-            }
 
-            return result;
+        /// <summary>
+        /// Test whether all array elements evaluate to True.
+        /// </summary>
+        /// <param name="nd"></param>
+        /// <returns></returns>
+        public static bool all(NDArray nd)
+        {
+            var data = nd.Storage.GetData<bool>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (!data[i])
+                    return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Test whether all array elements along a given axis evaluate to True.
+        /// </summary>
+        /// <param name="nd"></param>
+        /// <param name="axis"></param>
+        /// <returns>Returns an array of bools</returns>
+        public static NDArray<bool> all(NDArray nd, int axis)
+        {
+            throw new NotImplementedException($"np.all axis {axis}");
         }
     }
 }
