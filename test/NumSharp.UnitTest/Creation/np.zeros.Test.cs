90e4e5371a8f52d667af8667ead146186ab138a0
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-04-05 08:39:01 +0200

    np.ones and np.zeros: cleaned up test cases

--- test/NumSharp.UnitTest/Creation/np.zeros.Test.cs
+++ test/NumSharp.UnitTest/Creation/np.zeros.Test.cs
@@ -18,5 +18,41 @@ namespace NumSharp.UnitTest.Creation
 
             Assert.IsTrue(Enumerable.SequenceEqual(n.Storage.GetData<double>(), new double[] { 0, 0, 0 }));
         }
+        
+        [TestMethod]
+        public void Zeros2Dim()
+        {
+            var n = np.zeros(3, 2);
+            Assert.IsTrue(Enumerable.SequenceEqual(n.Storage.GetData<double>(), new double[] { 0, 0, 0, 0, 0, 0 }));
+        }
+
+        [TestMethod]
+        public void Zeros1DimWithDtype()
+        {
+            var n = np.zeros(new Shape(3), np.int32);
+            Assert.IsTrue(Enumerable.SequenceEqual(n.Storage.GetData<int>(), new int[] { 0, 0, 0 }));
+        }
+
+        [TestMethod]
+        public void SimpleInt1D()
+        {
+            var np1 = np.zeros(new Shape(5));
+
+            Assert.IsTrue(np1.Storage.GetData<double>().Where(x => x == 0).ToArray().Length == 5);
+        }
+        [TestMethod]
+        public void SimpleInt2D()
+        {
+            var np1 = np.zeros(new Shape(5, 5));
+
+            Assert.IsTrue(np1.Storage.GetData<double>().Where(x => x == 0).ToArray().Length == 25);
+        }
+        [TestMethod]
+        public void SimpleDouble3D()
+        {
+            var np1 = np.zeros(new Shape(5, 5, 5));
+
+            Assert.IsTrue(np1.Storage.GetData<double>().Where(x => x == 0).ToArray().Length == 125);
+        }
     }
 }
