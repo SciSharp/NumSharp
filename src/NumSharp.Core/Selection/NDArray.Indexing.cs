c7aa9564256ab7ea904616797064e590d1e40a28
haiping008 <haiping008@gmail.com>
2019-04-25 15:08:47 -0500

    optimize public NDArray this[NDArray indices]

--- src/NumSharp.Core/Selection/NDArray.Indexing.cs
+++ src/NumSharp.Core/Selection/NDArray.Indexing.cs
@@ -23,71 +23,38 @@ namespace NumSharp
             {
                 return Storage.GetData(select);
             }
-            
+
             set
             {
                 Storage.SetData(value, select);
             }
         }
-        
+
         public NDArray this[NDArray indices]
         {
             get
             {
                 NDArray nd = null;
 
-                switch (ndim)
+                switch (Type.GetTypeCode(dtype))
                 {
-                    case 1:
-                        if (dtype.Name == "Byte")
-                            nd = setValue1D<byte>(indices);
-                        else if (dtype.Name == "Int32")
-                            nd = setValue1D<int>(indices);
-                        else if (dtype.Name == "Int64")
-                            nd = setValue1D<long>(indices);
-                        else if (dtype.Name == "Single")
-                            nd = setValue1D<float>(indices);
-                        else if (dtype.Name == "Double")
-                            nd = setValue1D<double>(indices);
+                    case TypeCode.Byte:
+                        nd = setValue<byte>(indices);
                         break;
-
-                    case 2:
-                        if (dtype.Name == "Byte")
-                            nd = setValue2D<byte>(indices);
-                        else if (dtype.Name == "Int32")
-                            nd = setValue2D<int>(indices);
-                        else if (dtype.Name == "Int64")
-                            nd = setValue2D<long>(indices);
-                        else if (dtype.Name == "Single")
-                            nd = setValue2D<float>(indices);
-                        else if (dtype.Name == "Double")
-                            nd = setValue2D<double>(indices);
+                    case TypeCode.Int32:
+                        nd = setValue<int>(indices);
                         break;
-
-                    case 3:
-                        if (dtype.Name == "Byte")
-                            nd = setValue3D<byte>(indices);
-                        else if (dtype.Name == "Int32")
-                            nd = setValue3D<int>(indices);
-                        else if (dtype.Name == "Int64")
-                            nd = setValue3D<long>(indices);
-                        else if (dtype.Name == "Single")
-                            nd = setValue3D<float>(indices);
-                        else if (dtype.Name == "Double")
-                            nd = setValue3D<double>(indices);
+                    case TypeCode.Int64:
+                        nd = setValue<long>(indices);
                         break;
-
-                    case 4:
-                        if (dtype.Name == "Byte")
-                            nd = setValue4D<byte>(indices);
-                        else if (dtype.Name == "Int32")
-                            nd = setValue4D<int>(indices);
-                        else if (dtype.Name == "Int64")
-                            nd = setValue4D<long>(indices);
-                        else if (dtype.Name == "Single")
-                            nd = setValue4D<float>(indices);
-                        else if (dtype.Name == "Double")
-                            nd = setValue4D<double>(indices);
+                    case TypeCode.Single:
+                        nd = setValue<float>(indices);
+                        break;
+                    case TypeCode.Double:
+                        nd = setValue<double>(indices);
+                        break;
+                    case TypeCode.Decimal:
+                        nd = setValue<decimal>(indices);
                         break;
                 }
 
@@ -100,99 +67,30 @@ namespace NumSharp
             }
         }
 
-        private NDArray setValue1D<T>(NDArray indexes)
+        private NDArray setValue<T>(NDArray indexes)
         {
+            Shape newShape = new int[] { indexes.size }.Concat(shape.Skip(1)).ToArray();
             var buf = Data<T>();
             var idx = indexes.Data<int>();
-            var values = new T[indexes.size];
+            var array = new T[newShape.Size];
 
-            Parallel.For(0, indexes.size, (row) =>
+            var indice = Shape.GetShape(newShape.Dimensions, axis: 0);
+            var length = Shape.GetSize(indice);
+
+            for (var row = 0; row < newShape[0]; row++)
             {
-                values[row] = buf[idx[row]];
-            });
-
-            return new NDArray(values, indexes.size);
-        }
-
-        private NDArray setValue2D<T>(NDArray indexes)
-        {
-            var buf = Data<T>();
-            var idx = indexes.Data<int>();
-            var nd = new NDArray(dtype, new Shape(indexes.size, shape[1]));
-
-            Parallel.For(0, nd.shape[0], (row) =>
-            {
-                for (int col = 0; col < nd.shape[1]; col++)
-                    nd.SetData(buf[Storage.Shape.GetIndexInShape(idx[row], col)], row, col);
-            });
-
-            return indexes.ndim == 0 ? np.squeeze(nd) : nd;
-        }
-
-        private NDArray setValue3D<T>(NDArray indexes)
-        {
-            var buf = Data<T>();
-            var nd = new NDArray(dtype, new Shape(indexes.size, shape[1], shape[2]));
-            var idx = indexes.Data<int>();
-
-            Parallel.For(0, nd.shape[0], (item) =>
-            {
-                for (int row = 0; row < nd.shape[1]; row++)
-                    for (int col = 0; col < nd.shape[2]; col++)
-                        nd.SetData(buf[Storage.Shape.GetIndexInShape(idx[item], row, col)], item, row, col);
-            });
-
-            return nd;
-        }
-
-        private NDArray setValue4D<T>(NDArray indexes)
-        {
-            var nd = new NDArray(dtype, new Shape(indexes.size, shape[1], shape[2], shape[3]));
-
-            /*switch (Type.GetTypeCode(dtype))
-            {
-                case TypeCode.Byte:
-                    {
-                        var buf = Data<byte>().AsSpan();
-                        
-                        var idx = indexes.Data<int>().AsSpan();
-
-                        var shapes = nd.shape.AsSpan();
-                        var data = nd.Data<byte>().AsSpan();
-
-                        Parallel.For(0, nd.shape[0], (item) =>
-                        {
-                            for (int row = 0; row < shapes[1]; row++)
-                                for (int col = 0; col < shapes[2]; col++)
-                                    for (int channel = 0; channel < shapes[3]; channel++)
-                                        data[Storage.Shape.GetIndexInShape(item, row, col, channel)] = buf[Storage.Shape.GetIndexInShape(idx[item], row, col, channel)];
-                        });
-                    }
-                    break;
-            }*/
-
-            {
-                var buf = Data<T>();
-                var idx = indexes.Data<int>();
-                var data = nd.Data<T>();
-
-                Parallel.For(0, nd.shape[0], (item) =>
-                {
-                    for (int row = 0; row < nd.shape[1]; row++)
-                        for (int col = 0; col < nd.shape[2]; col++)
-                            for (int channel = 0; channel < nd.shape[3]; channel++)
-                                data[Storage.Shape.GetIndexInShape(item, row, col, channel)] = buf[Storage.Shape.GetIndexInShape(idx[item], row, col, channel)];
-                });
+                var d = buf.AsSpan(idx[row] * length, length);
+                d.CopyTo(array.AsSpan(row * length));
             }
 
-            return nd;
+            return new NDArray(array, newShape);
         }
 
         public NDArray this[NDArray<bool> booleanArray]
         {
             get
             {
-                if (!Enumerable.SequenceEqual(shape,booleanArray.shape))
+                if (!Enumerable.SequenceEqual(shape, booleanArray.shape))
                 {
                     throw new IncorrectShapeException();
                 }
@@ -236,7 +134,7 @@ namespace NumSharp
             }
             set
             {
-                if (!Enumerable.SequenceEqual(shape,booleanArray.shape))
+                if (!Enumerable.SequenceEqual(shape, booleanArray.shape))
                 {
                     throw new IncorrectShapeException();
                 }
@@ -247,7 +145,7 @@ namespace NumSharp
 
                 int elementsAmount = booleanArray.size;
 
-                for(int idx = 0; idx < elementsAmount;idx++)
+                for (int idx = 0; idx < elementsAmount; idx++)
                 {
                     if (boolDotNetArray[idx])
                     {
@@ -256,10 +154,7 @@ namespace NumSharp
                     }
                 }
 
-            } 
+            }
         }
-      
     }
-
-    
 }
