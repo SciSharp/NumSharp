5d68beb1aa11d36dd0f38defb9fac5af0255d0cc
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-05-06 09:41:16 +0200

    NDArray.ToString() can now print N-dimensional shapes

--- src/NumSharp.Core/Casting/NdArray.ToString.cs
+++ src/NumSharp.Core/Casting/NdArray.ToString.cs
@@ -3,6 +3,7 @@ using NumSharp;
 using System.Linq;
 using System.Globalization;
 using System.Collections.Generic;
+using System.Text;
 
 namespace NumSharp
 {
@@ -10,17 +11,71 @@ namespace NumSharp
     {
         public override string ToString()
         {
-            string output = "";
-
-            if (ndim == 0)
-                output = Storage.GetData().GetValue(0).ToString();
-            else if (ndim == 2)
-                output = _ToMatrixString();
-            else
-                output = _ToVectorString();
-
-            return output;
+            return ToString(flat:false);
         }
+
+        public string ToString(bool flat)
+        {
+            var s = new StringBuilder();
+            if (shape.Length == 0)
+            {
+                s.Append($"{Storage.GetData().GetValue(0)}");
+            }
+            else
+            {
+                s.Append("array(");
+                PrettyPrint(s, flat);
+                s.Append(")");
+            }
+            return s.ToString();
+        }
+
+        private void PrettyPrint(StringBuilder s, bool flat = false)
+        {
+            if (shape.Length == 0)
+            {
+                s.Append($"{Storage.GetData().GetValue(0)}");
+                return;
+            }
+            if (shape.Length == 1)
+            {
+                s.Append("[");
+                s.Append(string.Join(", ", this.Array.OfType<object>().Select(x => x == null ? "null" : x.ToString())));
+                s.Append("]");
+                return;
+            }
+            var size = shape[0];
+            s.Append("[");
+            for (int i = 0; i < size; i++)
+            {
+                var n_minus_one_dim_slice = this[Slice.Index(i)];
+                n_minus_one_dim_slice.PrettyPrint(s, flat);
+                if (i < size - 1)
+                {
+                    s.Append(", ");
+                    if (!flat)
+                        s.AppendLine();
+                }
+            }
+            s.Append("]");
+        }
+        
+        //public override string ToString()
+        //{
+        //    if (Storage is ViewStorage)
+        //        return $"array({(Storage as ViewStorage).ToString()})";
+        //    string output = "";
+
+        //    if (ndim == 0)
+        //        output = Storage.GetData().GetValue(0).ToString();
+        //    else if (ndim == 2)
+        //        output = _ToMatrixString();
+        //    else
+        //        output = _ToVectorString();
+
+        //    return output;
+        //}
+                
         protected string _ToVectorString()
         {
             string returnValue = "array([";
@@ -150,6 +205,7 @@ namespace NumSharp
 
             return parsed;
         }
+
     }
     
 }
