68a221e4a2a6769b9719b3ec6c8c37b254f160e5
Eli Belash <elibelash@gmail.com>
2019-06-15 16:48:22 +0300

    Added docs for np.reshape

--- src/NumSharp.Core/Manipulation/np.reshape.cs
+++ src/NumSharp.Core/Manipulation/np.reshape.cs
@@ -6,6 +6,13 @@ namespace NumSharp
 {
     public static partial class np
     {
+        /// <summary>
+        ///     Gives a new shape to an array without changing its data.
+        /// </summary>
+        /// <param name="nd">Array to be reshaped.</param>
+        /// <param name="shape">The new shape should be compatible with the original shape. </param>
+        /// <returns>original <paramref name="nd"/> reshaped without copying.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy-1.16.0/reference/generated/numpy.reshape.html</remarks>
         public static NDArray reshape(NDArray nd, params int[] shape)
         {
             return nd.reshape(shape);
