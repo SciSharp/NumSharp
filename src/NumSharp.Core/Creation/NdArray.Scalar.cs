22d750a0a267a2d0c48980b014eb8576721dbdce
Eli Belash <elibelash@gmail.com>
2019-06-15 18:57:01 +0300

    Added null check to static ndarray.scalar

--- src/NumSharp.Core/Creation/NdArray.Scalar.cs
+++ src/NumSharp.Core/Creation/NdArray.Scalar.cs
@@ -15,8 +15,15 @@ namespace NumSharp
         /// <param name="dtype">The type of the scalar.</param>
         /// <returns></returns>
         /// <remarks>In case when <see cref="value"/> is not <see cref="dtype"/>, <see cref="Convert.ChangeType(object,System.Type)"/> will be called.</remarks>
+        /// <exception cref="ArgumentNullException">when dtype is null</exception>
+        /// <exception cref="InvalidCastException">This conversion is not supported. -or- value is null and conversionType is a value type. -or- value does not implement the IConvertible interface. </exception>
         public static NDArray Scalar(object value, Type dtype)
         {
+            if (dtype == null)
+            {
+                throw new ArgumentNullException(nameof(dtype));
+            }
+
             var ndArray = new NDArray(dtype, new int[0]);
             var arr = Array.CreateInstance(dtype, 1);
             if (dtype != value.GetType())
@@ -32,7 +39,6 @@ namespace NumSharp
         ///     Creates a scalar <see cref="NDArray"/> of <see cref="value"/> from a known type <typeparamref name="T"/>.
         /// </summary>
         /// <param name="value">The value of the scalar</param>
-        /// <param name="dtype">The type of the scalar.</param>
         /// <returns></returns>
         /// <remarks>In case when <see cref="value"/> is not <see cref="dtype"/>, <see cref="Convert.ChangeType(object,System.Type)"/> will be called.</remarks>
         public static NDArray Scalar<T>(T value)
