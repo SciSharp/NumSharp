69f332d8ab6a6b391334a61d93bc26384534713a
Eli Belash <elibelash@gmail.com>
2019-06-15 16:47:20 +0300

    Added static NDArray.Scalar method for creating scalar without using implicit casting.

---
+++ src/NumSharp.Core/Creation/NdArray.Scalar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace NumSharp
+{
+    public partial class NDArray
+    {
+        /// <summary>
+        ///     Creates a scalar <see cref="NDArray"/> of <see cref="value"/> and <see cref="dtype"/>.
+        /// </summary>
+        /// <param name="value">The value of the scalar</param>
+        /// <param name="dtype">The type of the scalar.</param>
+        /// <returns></returns>
+        /// <remarks>In case when <see cref="value"/> is not <see cref="dtype"/>, <see cref="Convert.ChangeType(object,System.Type)"/> will be called.</remarks>
+        public static NDArray Scalar(object value, Type dtype)
+        {
+            var ndArray = new NDArray(dtype, new int[0]);
+            var arr = Array.CreateInstance(dtype, 1);
+            if (dtype != value.GetType())
+                value = Convert.ChangeType(value, dtype);
+
+            arr.SetValue(value, 0);
+            ndArray.Storage.SetData(arr);
+
+            return ndArray;
+        }
+
+        /// <summary>
+        ///     Creates a scalar <see cref="NDArray"/> of <see cref="value"/> from a known type <typeparamref name="T"/>.
+        /// </summary>
+        /// <param name="value">The value of the scalar</param>
+        /// <param name="dtype">The type of the scalar.</param>
+        /// <returns></returns>
+        /// <remarks>In case when <see cref="value"/> is not <see cref="dtype"/>, <see cref="Convert.ChangeType(object,System.Type)"/> will be called.</remarks>
+        public static NDArray Scalar<T>(T value)
+        {
+            var ndArray = new NDArray(typeof(T), new int[0]);
+            ndArray.Storage.SetData(new T[] {value});
+
+            return ndArray;
+        }
+    }
+}
