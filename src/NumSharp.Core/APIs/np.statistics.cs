86b0f5076ef0d8a971bb9dc35ddd3793141ebb26
Eli Belash <elibelash@gmail.com>
2019-06-15 16:51:01 +0300

    Added docs for ndarray.mean

--- src/NumSharp.Core/APIs/np.statistics.cs
+++ src/NumSharp.Core/APIs/np.statistics.cs
@@ -19,6 +19,16 @@ namespace NumSharp
         public static NDArray max(NDArray nd, int axis)
             => nd.max(axis);
 
+        /// <summary>
+        ///     Compute the arithmetic mean along the specified axis.
+        ///     Returns the average of the array elements.
+        ///     The average is taken over the flattened array by default, otherwise over the specified axis.
+        ///     float64 intermediate and return values are used for integer inputs.
+        /// </summary>
+        /// <param name="nd"></param>
+        /// <param name="axis">Axis or axes along which the means are computed. The default is to compute the mean of the flattened array.</param>
+        /// <returns></returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy-1.16.1/reference/generated/numpy.mean.html</remarks>
         public static NDArray mean(NDArray nd, int axis = -1)
             => BackendFactory.GetEngine().Mean(nd, axis);
     }
