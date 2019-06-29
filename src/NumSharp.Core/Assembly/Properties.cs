e30d7f74ee3ed3b6613f55dc51ed002d14bd062a
Eli Belash <elibelash@gmail.com>
2019-06-29 03:58:43 +0300

    Added InternalsVisibleTo("TensorFlowNET.UnitTest")

--- src/NumSharp.Core/Assembly/Properties.cs
+++ src/NumSharp.Core/Assembly/Properties.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("NumSharp.UnitTest")]
 [assembly: InternalsVisibleTo("NumSharp.Benchmark")]
+[assembly: InternalsVisibleTo("TensorFlowNET.UnitTest")]
