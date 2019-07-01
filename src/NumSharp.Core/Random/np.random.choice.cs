3c56404f8ea1b7aa5ca1348855bf9c38e670dc66
Bjorn PersMats <bjorn@wavr.com>
2019-07-01 11:05:44 +0200

    Stub files for np.random.choice

---
+++ src/NumSharp.Core/Random/np.random.choice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumSharp
+{
+    public partial class NumPyRandom
+    {
+        /// <summary>
+        /// //todo
+        /// </summary>
+        /// <param name="arr">If an ndarray, a random sample is generated from its elements. If an int, the random sample is generated as if a were np.arange(a)</param>
+        /// <param name="shape"></param>
+        /// <param name="probabilities">The probabilities associated with each entry in a. If not given the sample assumes a uniform distribution over all entries in a.</param>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.random.choice.html</remarks>
+        public NDArray choice(NDArray arr, Shape shape, double[] probabilities = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        ///  //todo
+        /// </summary>
+        /// <param name="a">If an ndarray, a random sample is generated from its elements. If an int, the random sample is generated as if a were np.arange(a)</param>
+        /// <param name="shape"></param>
+        /// <param name="probabilities">The probabilities associated with each entry in a. If not given the sample assumes a uniform distribution over all entries in a.</param>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.random.choice.html</remarks>
+        public NDArray choice(int a, Shape shape, double[] probabilities = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/NumSharp.UnitTest/Random/np.random.choice.Test.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumSharp.UnitTest.RandomSampling
+{
+    [TestClass]
+    public class NpRandomChoiceTests : TestClass
+    {
+        [TestMethod]
+        public void HelloWorld()
+        {
+            Assert.Fail("Not implemented");
+        }
+    }
+}
