d9c67a5bd045aaf93ce52388511e478ce48b01c6
Bjorn P Mattsson <bjorn.pm@plankter.se>
2019-07-02 14:58:06 +0200

    Added simple implementation of cumsum

---
+++ src/NumSharp.Core/Math/NDArray.cumsum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumSharp
+{
+    public partial class NDArray
+    {
+        public NDArray cumsum()
+        {
+            return np.cumsum(this);
+        }
+    }
+
+    public static partial class np
+    {
+        public static NDArray cumsum(NDArray a)
+        {
+            // TODO currently no support for multidimensional a
+            NDArray cs = np.zeros(a.shape[0]);
+            cs[0] = a[0];
+            for (int i = 1; i < a.shape[0]; i++)
+            {
+                cs[i] = cs[i - 1] + a[i];
+            }
+            return cs;
+        }
+    }
+}
