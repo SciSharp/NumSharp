f5815cb994367d9947c3efa6086f0a42fa1b11a5
Bjorn P Mattsson <bjorn.pm@plankter.se>
2019-07-02 15:38:37 +0200

    Simple test for cumsum

---
+++ test/NumSharp.UnitTest/Math/NDArray.cumsum.Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumSharp.UnitTest.Maths
+{
+    [TestClass]
+    public class NDArrayCumsumTest : TestClass
+    {
+        [TestMethod]
+        public void CumsumStaticTest()
+        {
+            NDArray arr = new double[] { 0, 1, 4, 2, 5, 6, 2 };
+            NDArray expected = new double[] { 0, 1, 5, 7, 12, 18, 20 };
+
+            NDArray actual = np.cumsum(arr);
+
+            for (int i = 0; i < expected.shape[0]; i++)
+            {
+                Assert.AreEqual((double)expected[i], (double)actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void CumsumMemberTest()
+        {
+            NDArray arr = new double[] { 0, 1, 4, 2, 5, 6, 2 };
+            NDArray expected = new double[] { 0, 1, 5, 7, 12, 18, 20 };
+
+            NDArray actual = arr.cumsum();
+
+            for (int i = 0; i < expected.shape[0]; i++)
+            {
+                Assert.AreEqual((double)expected[i], (double)actual[i]);
+            }
+        }
+    }
+}
