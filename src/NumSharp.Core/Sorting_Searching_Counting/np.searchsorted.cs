1b5daa0b98872316c00d9934466e15faf0b003cf
Bjorn P Mattsson <bjorn.pm@plankter.se>
2019-07-04 16:29:44 +0200

    Added documentation to np.searchsorted

--- src/NumSharp.Core/Sorting_Searching_Counting/np.searchsorted.cs
+++ src/NumSharp.Core/Sorting_Searching_Counting/np.searchsorted.cs
@@ -6,6 +6,15 @@ namespace NumSharp
 {
     public static partial class np
     {
+        /// <summary>
+        /// Find indices where elements should be inserted to maintain order.
+        ///
+        /// Find the indices into a sorted array a such that, if the corresponding elements in v were inserted before the indices, the order of a would be preserved.
+        /// </summary>
+        /// <param name="a">Input array. Must be sorted in ascending order.</param>
+        /// <param name="v">Values to insert into a.</param>
+        /// <returns>Array of insertion points with the same shape as v.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.searchsorted.html</remarks>
         public static NDArray searchsorted(NDArray a, NDArray v)
         {
             // TODO currently no support for multidimensional a
@@ -19,6 +28,13 @@ namespace NumSharp
             return output;
         }
 
+        /// <summary>
+        /// Find the (right-most) position of a target value within a sorted array.
+        /// </summary>
+        /// <param name="arr">Sorted array.</param>
+        /// <param name="target">Target value to find position of.</param>
+        /// <returns>Would-be index of target value within the array.</returns>
+        /// <remarks>https://en.wikipedia.org/wiki/Binary_search_algorithm</remarks>
         private static int binarySearchRightmost(NDArray arr, double target)
         {
             // TODO should probably not work on NDArray? Does it make sense to do binary search on multidimensional arrays?
