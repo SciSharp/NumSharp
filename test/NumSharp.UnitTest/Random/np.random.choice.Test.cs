d598199c97438ca00b3cdb10fe96602dbfcfb3cc
Bjorn P Mattsson <bjorn.pm@plankter.se>
2019-07-04 16:17:46 +0200

    Improved unit tests for np.random.choice

--- test/NumSharp.UnitTest/Random/np.random.choice.Test.cs
+++ test/NumSharp.UnitTest/Random/np.random.choice.Test.cs
@@ -10,7 +10,7 @@ namespace NumSharp.UnitTest.RandomSampling
     public class NpRandomChoiceTests : TestClass
     {
         [TestMethod]
-        public void UniformSample1()
+        public void UniformOneSample()
         {
             // Generate a uniform random sample from np.arange(5) of size 1:
             // This is equivalent to np.random.randint(0,5,1)
@@ -18,18 +18,19 @@ namespace NumSharp.UnitTest.RandomSampling
             int high = 5;
             int nrSamples = 1;
 
-            NDArray arr = np.random.choice(high); // Not specifying size means 1 single value is wanted
-            Assert.AreEqual(arr.len, nrSamples, "Unexpected number of elements");
+            NDArray actual = np.random.choice(high); // Not specifying size means 1 single value is wanted
+
+            Assert.AreEqual(actual.len, nrSamples, "Unexpected number of elements");
 
             // Verify that all elements in output are within the range
-            for (int i = 0; i < arr.len; i++)
+            for (int i = 0; i < actual.len; i++)
             {
-                Assert.IsTrue(arr[i] >= low, "Element was less than expected");
-                Assert.IsTrue(arr[i] < high, "Element was greater than expected");
+                Assert.IsTrue(actual[i] >= low, "Element was less than expected");
+                Assert.IsTrue(actual[i] < high, "Element was greater than expected");
             }
         }
         [TestMethod]
-        public void UniformSample2()
+        public void UniformMultipleSample()
         {
             // Generate a uniform random sample from np.arange(5) of size 3:
             // This is equivalent to np.random.randint(0,5,3)
@@ -37,19 +38,20 @@ namespace NumSharp.UnitTest.RandomSampling
             int high = 5;
             int nrSamples = 3;
 
-            NDArray arr = np.random.choice(high, nrSamples);
-            Assert.AreEqual(arr.len, nrSamples, "Unexpected number of elements");
+            NDArray actual = np.random.choice(high, nrSamples);
+
+            Assert.AreEqual(actual.len, nrSamples, "Unexpected number of elements");
 
             // Verify that all elements in output are within the range
-            for (int i = 0; i < arr.len; i++)
+            for (int i = 0; i < actual.len; i++)
             {
-                Assert.IsTrue(arr[i] >= low, "Element was less than expected");
-                Assert.IsTrue(arr[i] < high, "Element was greater than expected");
+                Assert.IsTrue(actual[i] >= low, "Element was less than expected");
+                Assert.IsTrue(actual[i] < high, "Element was greater than expected");
             }
         }
 
         [TestMethod]
-        public void NonUniformSample1()
+        public void NonUniformSample()
         {
             // Generate a non-uniform random sample from np.arange(5) of size 3:
             int low = 0;
@@ -57,22 +59,23 @@ namespace NumSharp.UnitTest.RandomSampling
             int nrSamples = 3;
             double[] probabilities = new double[] {0.1, 0, 0.3, 0.6, 0};
 
-            NDArray arr = np.random.choice(5, nrSamples, probabilities: probabilities);
-            Assert.AreEqual(arr.len, nrSamples, "Unexpected number of elements");
+            NDArray actual = np.random.choice(5, nrSamples, probabilities: probabilities);
+
+            Assert.AreEqual(actual.len, nrSamples, "Unexpected number of elements");
 
             // Verify that all elements in output are within the range
-            for (int i = 0; i < arr.len; i++)
+            for (int i = 0; i < actual.len; i++)
             {
-                Assert.IsTrue(arr[i] >= low, "Element was less than expected");
-                Assert.IsTrue(arr[i] < high, "Element was greater than expected");
-                Assert.IsTrue(arr[i] != 1, "Sampled zero-probability element");
-                Assert.IsTrue(arr[i] != 4, "Sampled zero-probability element");
+                Assert.IsTrue(actual[i] >= low, "Element was less than expected");
+                Assert.IsTrue(actual[i] < high, "Element was greater than expected");
+                Assert.IsTrue(actual[i] != 1, "Sampled zero-probability element");
+                Assert.IsTrue(actual[i] != 4, "Sampled zero-probability element");
             }
         }
 
         [TestMethod]
         [Ignore("Choice without replacement not implemented yet")]
-        public void UniformSampleWithoutReplace1()
+        public void UniformSampleWithoutReplace()
         {
             NDArray actual = np.random.choice(5, 3, replace: false);
             Assert.Fail("Not implemented");
@@ -80,7 +83,7 @@ namespace NumSharp.UnitTest.RandomSampling
 
         [TestMethod]
         [Ignore("Choice without replacement not implemented yet")]
-        public void NonUniformSampleWithoutReplace1()
+        public void NonUniformSampleWithoutReplace()
         {
             double[] probabilities = new double[] { 0.1, 0, 0.3, 0.6, 0 };
             NDArray actual = np.random.choice(5, 3, replace: false, probabilities: probabilities);
@@ -88,13 +91,42 @@ namespace NumSharp.UnitTest.RandomSampling
         }
 
         [TestMethod]
-        [Ignore("Choice with arbitrary arrays not implemented yet")]
-        public void NonUniformArraySample1()
+        [Ignore("Choice with string arrays not implemented yet")]
+        public void StringArraySample1()
         {
+            int nrSamples = 5;
+
             NDArray aa_milne_arr = new string[] { "pooh", "rabbit", "piglet", "Christopher" };
-            double[] probabilities = new double[] { 0.1, 0, 0.3, 0.6, 0 };
-            NDArray actual = np.random.choice(aa_milne_arr, 5, probabilities: probabilities);
-            Assert.Fail("Not implemented");
+            double[] probabilities = new double[] { 0.5, 0.1, 0.0, 0.3 };
+
+            NDArray actual = np.random.choice(aa_milne_arr, nrSamples, probabilities: probabilities);
+
+            Assert.AreEqual(actual.len, nrSamples, "Unexpected number of elements");
+
+            // Verify that all elements in output are within the possibilities
+            for (int i = 0; i < actual.len; i++)
+            {
+                Assert.IsTrue((string)actual[i] != (string)aa_milne_arr[2], "Sampled zero-probability element");
+            }
+        }
+
+        [TestMethod]
+        public void IntegerArraySample()
+        {
+            int nrSamples = 5;
+
+            NDArray int_arr = new int[] { 42, 96, 3, 101 };
+            double[] probabilities = new double[] { 0.5, 0.1, 0.0, 0.3 };
+
+            NDArray actual = np.random.choice(int_arr, nrSamples, probabilities: probabilities);
+
+            Assert.AreEqual(actual.len, nrSamples, "Unexpected number of elements");
+
+            // Verify that all elements in output are within the possibilities
+            for (int i = 0; i < actual.len; i++)
+            {
+                Assert.IsTrue((int)actual[i] != (int)int_arr[2], "Sampled zero-probability element");
+            }
         }
     }
 }
