cf5821677d5b47f1f65ada9754bc549d467381c7
Eli Belash <elibelash@gmail.com>
2019-07-11 01:42:33 +0300

    Added MaxValue, MinValue and Zero to InfoOf<T>

--- src/NumSharp.Core/Utilities/InfoOf.cs
+++ src/NumSharp.Core/Utilities/InfoOf.cs
@@ -10,6 +10,16 @@ namespace NumSharp.Utilities
     internal class InfoOf<T>
     {
         public static readonly int Size = Marshal.SizeOf<T>();
-        public static readonly NPTypeCode NPTypeCode = typeof(T).GetTypeCode();
+        public static readonly NPTypeCode NPTypeCode;
+        public static readonly T Zero = default;
+        public static readonly T MaxValue;
+        public static readonly T MinValue;
+
+        static InfoOf()
+        {
+            NPTypeCode = typeof(T).GetTypeCode();
+            MaxValue = (T)NPTypeCode.MaxValue();
+            MinValue = (T)NPTypeCode.MinValue();
+        }
     }
 }
