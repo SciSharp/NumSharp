ddb0b722aa2f328a831aceaf768ae53fe0466da5
Eli Belash <elibelash@gmail.com>
2019-07-10 03:50:24 +0300

    Seperated static UnmanagedArray to a seperate file

---
+++ src/NumSharp.Core/Backends/Unmanaged/UnmanagedArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace NumSharp.Backends.Unmanaged {
+    public static class UnmanagedArray
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        /// <remarks>Returns a copy.</remarks>
+        [MethodImpl((MethodImplOptions)768)]
+        public static UnmanagedArray<TOut> Cast<TIn, TOut>(this UnmanagedArray<TIn> source) where TIn : unmanaged where TOut : unmanaged
+        {
+            unsafe
+            {
+                var ret = new UnmanagedArray<TOut>(source.Count);
+                var src = source.Address;
+                var dst = ret.Address;
+                var len = source.Count;
+                var tc = Type.GetTypeCode(typeof(TOut));
+                for (int i = 0; i < len; i++)
+                {
+                    *(dst + i) = (TOut)Convert.ChangeType((object)*(src + i), tc);
+                } //TODO! seperate class for NP!
+
+                return ret;
+            }
+        }
+    }
+}
--- src/NumSharp.Core/Backends/Unmanaged/UnmanagedArray`1.cs
+++ src/NumSharp.Core/Backends/Unmanaged/UnmanagedArray`1.cs
@@ -10,34 +10,6 @@ using OOMath.MemoryPooling;
 
 namespace NumSharp.Backends.Unmanaged
 {
-    public static class UnmanagedArray
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="source"></param>
-        /// <returns></returns>
-        /// <remarks>Returns a copy.</remarks>
-        [MethodImpl((MethodImplOptions)768)]
-        public static UnmanagedArray<TOut> Cast<TIn, TOut>(UnmanagedArray<TIn> source) where TIn : unmanaged where TOut : unmanaged
-        {
-            unsafe
-            {
-                var ret = new UnmanagedArray<TOut>(source.Count);
-                var src = source.Address;
-                var dst = ret.Address;
-                var len = source.Count;
-                var tc = Type.GetTypeCode(typeof(TOut));
-                for (int i = 0; i < len; i++)
-                {
-                    *(dst + i) = (TOut)Convert.ChangeType((object)*(src + i), tc);
-                } //TODO! support NPTypeCode
-
-                return ret;
-            }
-        }
-    }
-
     [StructLayout(LayoutKind.Sequential)]
     public unsafe struct UnmanagedArray<T> : IEnumerable<T>, IEquatable<UnmanagedArray<T>>, ICollection<T>, IUnmanagedArray where T : unmanaged
     {
