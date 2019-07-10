601f422d8aea4eaa3bf63901012da528f80116fb
Eli Belash <elibelash@gmail.com>
2019-07-11 01:37:37 +0300

    Further cleanup

--- src/NumSharp.Core/Extensions/LinqExtensions.cs
+++ src/NumSharp.Core/Extensions/LinqExtensions.cs
@@ -1,11 +1,11 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Drawing;
 
-namespace NumSharp
+namespace NumSharp.Extensions
 {
     public static class LinqExtensions
     {
--- src/NumSharp.Core/Sorting_Searching_Counting/ndarray.argsort.cs
+++ src/NumSharp.Core/Sorting_Searching_Counting/ndarray.argsort.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NumSharp.Extensions;
 
 namespace NumSharp
 {
