a342087038175f582dc32b065df5bbe4cfcc508c
Eli Belash <elibelash@gmail.com>
2019-07-11 14:07:48 +0300

    Added more properties to InfoOf<T>

--- src/NumSharp.Core/Utilities/InfoOf.cs
+++ src/NumSharp.Core/Utilities/InfoOf.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
 using NumSharp.Backends;
 
 namespace NumSharp.Utilities
@@ -7,19 +9,26 @@ namespace NumSharp.Utilities
     ///     Provides a cache for properties of <typeparamref name="T"/> that requires computation.
     /// </summary>
     /// <typeparam name="T"></typeparam>
+    [SuppressMessage("ReSharper", "StaticMemberInGenericType")]
     internal class InfoOf<T>
     {
-        public static readonly int Size = Marshal.SizeOf<T>();
+        public static readonly int Size;
         public static readonly NPTypeCode NPTypeCode;
-        public static readonly T Zero = default;
+        public static readonly T Zero;
         public static readonly T MaxValue;
         public static readonly T MinValue;
 
         static InfoOf()
         {
             NPTypeCode = typeof(T).GetTypeCode();
-            MaxValue = (T)NPTypeCode.MaxValue();
-            MinValue = (T)NPTypeCode.MinValue();
+            Size = Marshal.SizeOf<T>();
+            Zero = default;
+            try
+            {
+                MaxValue = (T)NPTypeCode.MaxValue();
+                MinValue = (T)NPTypeCode.MinValue();
+            }
+            catch (ArgumentOutOfRangeException) { }
         }
     }
 }
