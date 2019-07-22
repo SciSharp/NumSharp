18c95560cacb61f9ae68b7ec451604304f66076c
Eli Belash <elibelash@gmail.com>
2019-07-22 11:47:20 +0300

    Revamped np.linspace

--- src/NumSharp.Core/Creation/np.linspace.cs
+++ src/NumSharp.Core/Creation/np.linspace.cs
@@ -2,38 +2,440 @@
 using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
+using NumSharp.Backends;
 
 namespace NumSharp
 {
     public static partial class np
     {
-        public static NDArray linspace(double start, double stop, int num, bool entdpoint = true, Type dtype = null)
+        /// <summary>
+        ///     Return evenly spaced numbers over a specified interval.<br></br>
+        ///     Returns num evenly spaced samples, calculated over the interval[start, stop].<br></br>
+        ///     The endpoint of the interval can optionally be excluded.
+        /// </summary>
+        /// <param name="start">The starting value of the sequence.</param>
+        /// <param name="stop">The end value of the sequence, unless endpoint is set to False. In that case, the sequence consists of all but the last of num + 1 evenly spaced samples, so that stop is excluded. Note that the step size changes when endpoint is False.</param>
+        /// <param name="num">Number of samples to generate. Default is 50. Must be non-negative.</param>
+        /// <param name="endpoint">If True, stop is the last sample. Otherwise, it is not included. Default is True.</param>
+        /// <param name="dtype">The type of the output array. If dtype is not given, infer the data type from the other input arguments.</param>
+        /// <remarks>https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.linspace.html</remarks>
+        public static NDArray linspace(double start, double stop, int num, bool endpoint, Type dtype)
         {
-            dtype = (dtype == null) ? typeof(double) : dtype;
-
-            return new NDArray(dtype).linspace(start, stop, num, entdpoint);
+            return linspace(start, stop, num, endpoint, (dtype ?? typeof(double)).GetTypeCode());
         }
 
-        public static NDArray linspace(float start, float stop, int num, bool entdpoint = true, Type dtype = null)
+        /// <summary>
+        ///     Return evenly spaced numbers over a specified interval.<br></br>
+        ///     Returns num evenly spaced samples, calculated over the interval[start, stop].<br></br>
+        ///     The endpoint of the interval can optionally be excluded.
+        /// </summary>
+        /// <param name="start">The starting value of the sequence.</param>
+        /// <param name="stop">The end value of the sequence, unless endpoint is set to False. In that case, the sequence consists of all but the last of num + 1 evenly spaced samples, so that stop is excluded. Note that the step size changes when endpoint is False.</param>
+        /// <param name="num">Number of samples to generate. Default is 50. Must be non-negative.</param>
+        /// <param name="endpoint">If True, stop is the last sample. Otherwise, it is not included. Default is True.</param>
+        /// <param name="dtype">The type of the output array. If dtype is not given, infer the data type from the other input arguments.</param>
+        /// <remarks>https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.linspace.html</remarks>
+        public static NDArray linspace(float start, float stop, int num, bool endpoint, Type dtype)
         {
-            dtype = (dtype == null) ? typeof(float) : dtype;
-
-            return new NDArray(dtype).linspace(start, stop, num, entdpoint);
+            return linspace(start, stop, num, endpoint, (dtype ?? typeof(float)).GetTypeCode());
         }
 
-        public static NDArray linspace<T>(double start, double stop, int num, bool entdpoint = true)
+        /// <summary>
+        ///     Return evenly spaced numbers over a specified interval.<br></br>
+        ///     Returns num evenly spaced samples, calculated over the interval[start, stop].<br></br>
+        ///     The endpoint of the interval can optionally be excluded.
+        /// </summary>
+        /// <param name="start">The starting value of the sequence.</param>
+        /// <param name="stop">The end value of the sequence, unless endpoint is set to False. In that case, the sequence consists of all but the last of num + 1 evenly spaced samples, so that stop is excluded. Note that the step size changes when endpoint is False.</param>
+        /// <param name="num">Number of samples to generate. Default is 50. Must be non-negative.</param>
+        /// <param name="endpoint">If True, stop is the last sample. Otherwise, it is not included. Default is True.</param>
+        /// <param name="typeCode">The type of the output array. If dtype is not given, infer the data type from the other input arguments.</param>
+        /// <remarks>https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.linspace.html</remarks>
+        public static NDArray linspace(double start, double stop, int num, bool endpoint = true, NPTypeCode typeCode = NPTypeCode.Double)
         {
-            double steps = (stop - start) / ((entdpoint) ? (double)num - 1.0 : (double)num);
+            if (typeCode == NPTypeCode.Empty)
+                throw new ArgumentException("Invalid typeCode", nameof(typeCode));
 
-            double[] doubleArray = new double[num];
+            NDArray ret = new NDArray(typeCode, new Shape(num), false);
+            double step = (stop - start) / (endpoint ? num - 1.0 : num);
 
-            for (int idx = 0; idx < doubleArray.Length; idx++)
-                doubleArray[idx] = start + idx * steps;
+            switch (ret.GetTypeCode)
+            {
+#if _REGEN
+	            case NPTypeCode.Boolean:
+	            {
+                    unsafe
+                    {
+                        var addr = (bool*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = (start + i * step) != 0;
+                    }
 
-            var nd = new NDArray(typeof(T), new Shape(doubleArray.Length));
-            nd.SetData(doubleArray);
+                    return ret;
+	            }
+	            %foreach except(supported_currently_supported, "Boolean"),except(supported_currently_supported_lowercase, "bool")%
+	            case NPTypeCode.#1:
+	            {
+                    unsafe
+                    {
+                        var addr = (#2*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.To#1(start + i * step);
+                    }
 
-            return nd;
+                    return ret;
+	            }
+	            %
+	            default:
+		            throw new NotSupportedException();
+#else
+                case NPTypeCode.Boolean:
+                {
+                    unsafe
+                    {
+                        var addr = (bool*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = start + i * step != 0;
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Byte:
+                {
+                    unsafe
+                    {
+                        var addr = (byte*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToByte(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Int16:
+                {
+                    unsafe
+                    {
+                        var addr = (short*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToInt16(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.UInt16:
+                {
+                    unsafe
+                    {
+                        var addr = (ushort*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToUInt16(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Int32:
+                {
+                    unsafe
+                    {
+                        var addr = (int*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToInt32(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.UInt32:
+                {
+                    unsafe
+                    {
+                        var addr = (uint*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToUInt32(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Int64:
+                {
+                    unsafe
+                    {
+                        var addr = (long*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToInt64(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.UInt64:
+                {
+                    unsafe
+                    {
+                        var addr = (ulong*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToUInt64(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Char:
+                {
+                    unsafe
+                    {
+                        var addr = (char*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToChar(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Double:
+                {
+                    unsafe
+                    {
+                        var addr = (double*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToDouble(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Single:
+                {
+                    unsafe
+                    {
+                        var addr = (float*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToSingle(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Decimal:
+                {
+                    unsafe
+                    {
+                        var addr = (decimal*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToDecimal(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                default:
+                    throw new NotSupportedException();
+#endif
+            }
+        }
+
+        /// <summary>
+        ///     Return evenly spaced numbers over a specified interval.<br></br>
+        ///     Returns num evenly spaced samples, calculated over the interval[start, stop].<br></br>
+        ///     The endpoint of the interval can optionally be excluded.
+        /// </summary>
+        /// <param name="start">The starting value of the sequence.</param>
+        /// <param name="stop">The end value of the sequence, unless endpoint is set to False. In that case, the sequence consists of all but the last of num + 1 evenly spaced samples, so that stop is excluded. Note that the step size changes when endpoint is False.</param>
+        /// <param name="num">Number of samples to generate. Default is 50. Must be non-negative.</param>
+        /// <param name="endpoint">If True, stop is the last sample. Otherwise, it is not included. Default is True.</param>
+        /// <param name="typeCode">The type of the output array. If dtype is not given, infer the data type from the other input arguments.</param>
+        /// <remarks>https://docs.scipy.org/doc/numpy-1.15.0/reference/generated/numpy.linspace.html</remarks>
+        public static NDArray linspace(float start, float stop, int num, bool endpoint = true, NPTypeCode typeCode = NPTypeCode.Single)
+        {
+            if (typeCode == NPTypeCode.Empty)
+                throw new ArgumentException("Invalid typeCode", nameof(typeCode));
+
+            NDArray ret = new NDArray(typeCode, new Shape(num), false);
+            float step = Convert.ToSingle((stop - start) / (endpoint ? num - 1.0 : num));
+
+            switch (ret.GetTypeCode)
+            {
+#if _REGEN
+	            case NPTypeCode.Boolean:
+	            {
+                    unsafe
+                    {
+                        var addr = (bool*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = (start + i * step) != 0;
+                    }
+
+                    return ret;
+	            }
+	            %foreach except(supported_currently_supported, "Boolean"),except(supported_currently_supported_lowercase, "bool")%
+	            case NPTypeCode.#1:
+	            {
+                    unsafe
+                    {
+                        var addr = (#2*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.To#1(start + i * step);
+                    }
+
+                    return ret;
+	            }
+	            %
+	            default:
+		            throw new NotSupportedException();
+#else
+                case NPTypeCode.Boolean:
+                {
+                    unsafe
+                    {
+                        var addr = (bool*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = start + i * step != 0;
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Byte:
+                {
+                    unsafe
+                    {
+                        var addr = (byte*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToByte(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Int16:
+                {
+                    unsafe
+                    {
+                        var addr = (short*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToInt16(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.UInt16:
+                {
+                    unsafe
+                    {
+                        var addr = (ushort*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToUInt16(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Int32:
+                {
+                    unsafe
+                    {
+                        var addr = (int*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToInt32(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.UInt32:
+                {
+                    unsafe
+                    {
+                        var addr = (uint*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToUInt32(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Int64:
+                {
+                    unsafe
+                    {
+                        var addr = (long*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToInt64(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.UInt64:
+                {
+                    unsafe
+                    {
+                        var addr = (ulong*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToUInt64(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Char:
+                {
+                    unsafe
+                    {
+                        var addr = (char*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToChar(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Double:
+                {
+                    unsafe
+                    {
+                        var addr = (double*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToDouble(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Single:
+                {
+                    unsafe
+                    {
+                        var addr = (float*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToSingle(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                case NPTypeCode.Decimal:
+                {
+                    unsafe
+                    {
+                        var addr = (decimal*)ret.Address;
+                        for (int i = 0; i < num; i++)
+                            *(addr + i) = Convert.ToDecimal(start + i * step);
+                    }
+
+                    return ret;
+                }
+
+                default:
+                    throw new NotSupportedException();
+#endif
+            }
         }
     }
 }
