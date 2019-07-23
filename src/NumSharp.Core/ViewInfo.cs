72debc86c8900e8d85deb35695ac9f90b9dfa257
Meinrad Recheis <meinrad.recheis@gmail.com>
2019-07-23 14:15:53 +0200

    Shape: added ViewInfo which is needed for Slicing

---
+++ src/NumSharp.Core/ViewInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumSharp
+{
+    public class ViewInfo
+    {
+        public Slice[] Slices;
+        public Shape OriginalShape;
+        public Shape UnreducedShape;
+    }
+}
