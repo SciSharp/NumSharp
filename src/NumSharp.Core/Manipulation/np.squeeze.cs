84383a3f6196a6a07eb3a833272a8404e4c01da4
Eli Belash <elibelash@gmail.com>
2019-07-24 12:44:00 +0300

    Revamped np.squeeze

--- src/NumSharp.Core/Manipulation/np.squeeze.cs
+++ src/NumSharp.Core/Manipulation/np.squeeze.cs
@@ -4,20 +4,44 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using NumSharp.Utilities;
 
 namespace NumSharp
 {
     public static partial class np
     {
         /// <summary>
-        /// Remove single-dimensional entries from the shape of an array.
+        ///     Remove single-dimensional entries from the shape of an array.
         /// </summary>
-        /// <param name="nd"></param>
-        /// <param name="axis"></param>
-        /// <returns></returns>
-        public static NDArray squeeze(NDArray nd, int axis = -1)
+        /// <param name="a">Input data.</param>
+        /// <returns>The input array, but with all or a subset of the dimensions of length 1 removed. This is always a itself or a view into a.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.squeeze.html</remarks>
+        public static NDArray squeeze(NDArray a)
         {
-            return nd.reshape(nd.shape.Where(x => x > 1).ToArray());
+            return a.reshape(a.shape.Where(x => x != 1).ToArray());
+        }
+
+        /// <summary>
+        ///     Remove single-dimensional entries from the shape of an array.
+        /// </summary>
+        /// <param name="a">Input data.</param>
+        /// <param name="axis">Selects a subset of the single-dimensional entries in the shape. If an axis is selected with shape entry greater than one, an error is raised.</param>
+        /// <returns>The input array, but with all or a subset of the dimensions of length 1 removed. This is always a itself or a view into a.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.squeeze.html</remarks>
+        /// <exception cref="IncorrectShapeException">If axis is not None, and an axis being squeezed is not of length 1</exception>
+        public static NDArray squeeze(NDArray a, int axis)
+        {
+            if (axis < 0)
+                axis = a.ndim + axis;
+
+            if (axis >= a.ndim)
+                throw new ArgumentOutOfRangeException(nameof(axis));
+
+            var shape = a.shape;
+            if (shape[axis] != 1)
+                throw new IncorrectShapeException($"Unable to squeeze axis {axis} because it is of length {shape[axis]} and not 1.");
+
+            return a.reshape(shape.RemoveAt(axis));
         }
     }
 }
