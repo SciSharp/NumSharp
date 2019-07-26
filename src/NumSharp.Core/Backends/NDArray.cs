3dcae1ab98538ca6d00d35d8ff146f674f2cc075
Eli Belash <elibelash@gmail.com>
2019-07-26 14:26:50 +0300

    Added NDArray.flat property

--- src/NumSharp.Core/Backends/NDArray.cs
+++ src/NumSharp.Core/Backends/NDArray.cs
@@ -265,6 +265,9 @@ namespace NumSharp
 
         public int[] strides => Storage.Shape.Strides;
 
+        //TODO! when reshaping a slice is done - this should not clone in case of a sliced shape.
+        public NDArray flat => Shape.IsSliced ? new NDArray(Storage.Clone(), Shape.Vector(size)) : new NDArray(Storage, Shape.Vector(size));
+
         internal Shape Shape
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
