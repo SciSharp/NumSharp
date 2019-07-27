66df528839b78da34112b29c6a669341590a07fc
Eli Belash <elibelash@gmail.com>
2019-07-27 04:10:39 +0300

    Added ViewInfo.Clone()

--- src/NumSharp.Core/ViewInfo.cs
+++ src/NumSharp.Core/ViewInfo.cs
@@ -4,10 +4,17 @@ using System.Text;
 
 namespace NumSharp
 {
-    public class ViewInfo
+    public class ViewInfo : ICloneable
     {
         public SliceDef[] Slices;
         public Shape OriginalShape;
         public Shape UnreducedShape;
+
+        public ViewInfo Clone()
+        {
+            return new ViewInfo() {Slices = (SliceDef[])Slices.Clone(), OriginalShape = OriginalShape.Clone(true, false), UnreducedShape = UnreducedShape.Clone(true, false),};
+        }
+
+        object ICloneable.Clone() => Clone();
     }
 }
