c146feeb01da03065f98f1ece9a35c74c5df8270
Eli Belash <elibelash@gmail.com>
2019-07-28 03:16:19 +0300

    Update IncorrectTypeException.cs

--- src/NumSharp.Core/Exceptions/IncorrectTypeException.cs
+++ src/NumSharp.Core/Exceptions/IncorrectTypeException.cs
@@ -6,5 +6,6 @@ namespace NumSharp
     public class IncorrectTypeException : System.Exception
     {
         public IncorrectTypeException() : base("This method does not work with this dtype or was not already implemented.") { }
+        public IncorrectTypeException(string message) : base(message) { }
     }
 }
