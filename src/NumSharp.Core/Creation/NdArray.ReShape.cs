17461065564c44e9d8ffd37e33d4e64a18cd3f1d
Roy Sprowl <roys@jetstreamsoftware.com>
2019-07-30 13:28:21 -0700

    Implement missing dimension reshape, incomplete implementation of amax

--- src/NumSharp.Core/Creation/NdArray.ReShape.cs
+++ src/NumSharp.Core/Creation/NdArray.ReShape.cs
@@ -41,83 +41,55 @@ namespace NumSharp
         /// <summary>
         ///     Gives a new shape to an array without changing its data.
         /// </summary>
-        /// <param name="shape">The new shape should be compatible with the original shape. If an integer, then the result will be a 1-D array of that length. One shape dimension can be -1. In this case, the value is inferred from the length of the array and remaining dimensions.</param>
-        /// <returns>This will be a new view object if possible; otherwise, it will be a copy. Note there is no guarantee of the memory layout (C- or Fortran- contiguous) of the returned array.</returns>
+        /// <param name="shape">The new shape should be compatible with the original shape. If an integer, then the result will be a 
+        /// 1-D array of that length. One shape dimension can be -1. In this case, the value is inferred from the length of the array 
+        /// and remaining dimensions.</param>
+        /// <returns>This will be a new view object if possible; otherwise, it will be a copy. Note there is no guarantee of the 
+        /// memory layout (C- or Fortran- contiguous) of the returned array.</returns>
         /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.reshape.html</remarks>
-        public NDArray reshape(params int[] shape)
+        public NDArray reshape(params int[] shapeIn)
         {
+            int[] shape = InferMissingDimension(shapeIn);
+
             if (shape.Length == 0 && size != 0)
                 throw new ArgumentException("Value cannot be an empty collection.", nameof(shape));
 
             return reshape(new Shape(shape));
         }
 
-        protected static int FindNegativeIndex(params int[] shape)
+        // TODO: Unit test reshape with missing dimension
+        int[] InferMissingDimension(int[] shape)
         {
-            var count = shape.Length;
-            var negOne = false;
             var indexOfNegOne = -1;
-            for (int i = 0; i < count; i++)
+            int product = 1;
+            for (int i = 0; i < shape.Length; i++)
             {
                 if (shape[i] == -1)
                 {
-                    if (negOne)
+                    if (indexOfNegOne != -1)
                         throw new ArgumentException("Only allowed to pass one shape dimension as -1");
-
-                    negOne = true;
                     indexOfNegOne = i;
                 }
+                else
+                {
+                    product *= shape[i];
+                }
             }
-
-            return indexOfNegOne;
-        }
-
-        protected static int[] CalculateNegativeShape(int negativeIndex, IList<int> currentShape, params int[] shapeParams)
-        {
-            var currentShapeCount = currentShape.Count;
-            var shapeParamCount = shapeParams.Length;
-            var newShape = new List<int>();
-            var curShapeVolume = currentShape.Aggregate((x, y) => x * y);
-            if (negativeIndex > -1)
+            if (indexOfNegOne == -1)
             {
-                int x = shapeParams[0];
-                int y = 0;
-                if (shapeParamCount >= 1)
-                    y = shapeParams[1];
-                if (shapeParamCount > 2)
-                    throw new ArgumentException("We cannot currently handle reshapes of more than 2 dimensions");
-
-                if (negativeIndex == 0 && shapeParamCount == 2)
-                {
-                    var mod = curShapeVolume % y == 0;
-                    if (!mod)
-                        throw new ArgumentException($"Wrong Reshape. {curShapeVolume} is not evenly divisible by {y}");
-                    else
-                    {
-                        var a = curShapeVolume / y;
-                        var b = y;
-                        newShape.Add(a);
-                        newShape.Add(b);
-                    }
-                }
-                else if (negativeIndex == 1 && shapeParamCount == 2)
-                {
-                    var mod = curShapeVolume % x == 0;
-                    if (!mod)
-                        throw new ArgumentException($"Wrong Reshape. {curShapeVolume} is not evenly divisible by {x}");
-                    else
-                    {
-                        var a = x;
-                        var b = curShapeVolume / x;
-                        newShape.Add(a);
-                        newShape.Add(b);
-                    }
-                }
+                return shape;
             }
             else
-                return currentShape.ToArray();
-
-            return newShape.ToArray();
+            {
+                int missingValue = this.size / product;
+                if (missingValue * product != this.size)
+                {
+                    throw new ArgumentException("Bad shape: missing dimension would have to be non-integer");
+                }
+                int[] result = (int[])shape.Clone();
+                result[indexOfNegOne] = missingValue;
+                return result;
+            }
         }
     }
 }
