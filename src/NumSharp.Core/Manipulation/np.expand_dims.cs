8bd0afff9c6725bbc7facd53e891636dc2c32503
Eli Belash <elibelash@gmail.com>
2019-07-31 15:14:41 +0300

    Revamped np.expand_dims

--- src/NumSharp.Core/Manipulation/np.expand_dims.cs
+++ src/NumSharp.Core/Manipulation/np.expand_dims.cs
@@ -10,31 +10,39 @@ namespace NumSharp
     {
         private static readonly int[] __one = new int[] {1};
 
+        /// <summary>
+        ///     Expand the shape of an array.
+        ///     Insert a new axis that will appear at the axis position in the expanded array shape.
+        /// </summary>
+        /// <param name="a">Input array.</param>
+        /// <param name="axis">Position in the expanded axes where the new axis is placed. In a similar manner like <see cref="List{T}.Insert"/></param>
+        /// <returns>View of a with the number of dimensions increased by one.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.expand_dims.html</remarks>
         public static NDArray expand_dims(NDArray a, int axis)
         {
             //test if the ndarray is empty.
             if (a.size == 0)
                 return a;
-            //TODO! this can be done by a simple insert to a list or copy from two arrays.
-
-            //we create an nd-array of the shape and then slice/split it on axis index.
-            var shape = np.array(a.shape);
-            var left = shape[$":{axis}"].Cast<int>();
-            var right = shape[$"{axis}:"].Cast<int>();
 
             //then we append a 1 dim between the slice/split.
-            return new NDArray(a.Storage.Alias(new Shape(_combineEnumerables(left, __one, right).ToArray())));
+            return new NDArray(a.Storage.Alias(expand_dims(a.Shape, axis)));
+        }        
+        
+        public static Shape expand_dims(Shape a, int axis)
+        {
+            //test if the ndarray is empty.
+            if (a.size == 0)
+                return a;
 
-            IEnumerable<T> _combineEnumerables<T>(params IEnumerable<T>[] arrays)
-            {
-                foreach (var arr in arrays)
-                {
-                    foreach (var val in arr)
-                    {
-                        yield return val;
-                    }
-                }
-            }
+            //handle negative axis
+            if (axis < 0)
+                axis = a.NDim + axis;
+
+            //we create an nd-array of the shape and then slice/split it on axis index.
+            var shape = a.dimensions.ToList();
+            shape.Insert(axis, 1);
+
+            return shape.ToArray();
         }
     }
 }
