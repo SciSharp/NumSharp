7818bff1a380628b76f64beebf6f667882d3b5ea
Eli Belash <elibelash@gmail.com>
2019-07-31 15:32:43 +0300

    Added docs for np.mgrid[...]

--- src/NumSharp.Core/Creation/NdArray.Mgrid.cs
+++ src/NumSharp.Core/Creation/NdArray.Mgrid.cs
@@ -7,9 +7,17 @@ namespace NumSharp
 {
     public partial class NDArray
     {
-        public (NDArray, NDArray) mgrid(NDArray nd2)
+        /// <summary>
+        ///     nd_grid instance which returns a dense multi-dimensional “meshgrid”.
+        ///     An instance of numpy.lib.index_tricks.nd_grid which returns an dense (or fleshed out) mesh-grid when indexed, so that each returned argument has the same shape.
+        ///     The dimensions and number of the output arrays are equal to the number of indexing dimensions.If the step length is not a complex number, then the stop is not inclusive.
+        /// </summary>
+        /// <param name="rhs"></param>
+        /// <returns>mesh-grid `ndarrays` all of the same dimensions</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.mgrid.html</remarks>
+        public (NDArray, NDArray) mgrid(NDArray rhs)
         {
-            return np.mgrid(this, nd2);
+            return np.mgrid(this, rhs);
         }
     }
 }
--- src/NumSharp.Core/Creation/np.mgrid.cs
+++ src/NumSharp.Core/Creation/np.mgrid.cs
@@ -10,6 +10,18 @@ namespace NumSharp
 {
     public static partial class np
     {
+
+        //TODO! implement mgrid that takes (string lhs, string rhs) and (Slice lhs, Slice rhs) fallbacking to each other.
+
+        /// <summary>
+        ///     nd_grid instance which returns a dense multi-dimensional “meshgrid”.
+        ///     An instance of numpy.lib.index_tricks.nd_grid which returns an dense (or fleshed out) mesh-grid when indexed, so that each returned argument has the same shape.
+        ///     The dimensions and number of the output arrays are equal to the number of indexing dimensions.If the step length is not a complex number, then the stop is not inclusive.
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>mesh-grid `ndarrays` all of the same dimensions</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.mgrid.html</remarks>
         public static (NDArray, NDArray) mgrid(NDArray lhs, NDArray rhs)
         {
             if (!(lhs.ndim == 1 || rhs.ndim == 1))
