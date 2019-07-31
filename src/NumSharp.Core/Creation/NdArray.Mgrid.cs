9f58b251489f7a4a5f3918911e492653bb571353
Eli Belash <elibelash@gmail.com>
2019-07-31 15:29:08 +0300

    swapped between NDArray.mgrid and np.mgrid

--- src/NumSharp.Core/Creation/NdArray.Mgrid.cs
+++ src/NumSharp.Core/Creation/NdArray.Mgrid.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using NumSharp;
 using NumSharp.Backends.Unmanaged;
 
@@ -8,34 +9,7 @@ namespace NumSharp
     {
         public (NDArray, NDArray) mgrid(NDArray nd2)
         {
-            if (!(this.ndim == 1 || nd2.ndim == 1))
-                throw new IncorrectShapeException();
-
-            IArraySlice nd1Data = this.Storage.GetData();
-            IArraySlice nd2Data = nd2.Storage.GetData();
-
-            int[] resultDims = new int[] { this.Storage.Shape.Dimensions[0], nd2.Storage.Shape.Dimensions[0] };
-
-            NDArray res1 = new NDArray(this.dtype, resultDims);
-            NDArray res2 = new NDArray(this.dtype, resultDims);
-
-            IArraySlice res1Arr = res1.Storage.GetData();
-            IArraySlice res2Arr = res2.Storage.GetData();
-
-            int counter = 0;
-
-            for (int row = 0; row < nd1Data.Count; row++)
-            {
-                for (int col = 0; col < nd2Data.Count; col++)
-                {
-                    res1Arr[counter] = nd1Data[row];
-                    res2Arr[counter] = nd2Data[col];
-                    counter++;
-                }
-            }
-
-
-            return (res1, res2);
+            return np.mgrid(this, nd2);
         }
     }
 }
--- src/NumSharp.Core/Creation/np.mgrid.cs
+++ src/NumSharp.Core/Creation/np.mgrid.cs
@@ -1,17 +1,46 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Drawing;
+using NumSharp.Backends.Unmanaged;
 
 namespace NumSharp
 {
     public static partial class np
     {
-        public static (NDArray, NDArray) mgrid(NDArray nd1, NDArray nd2)
+        public static (NDArray, NDArray) mgrid(NDArray lhs, NDArray rhs)
         {
-            return nd1.mgrid(nd2);
+            if (!(lhs.ndim == 1 || rhs.ndim == 1))
+                throw new IncorrectShapeException();
+
+            IArraySlice nd1Data = lhs.Storage.GetData();
+            IArraySlice nd2Data = rhs.Storage.GetData();
+
+            int[] resultDims = new int[] { lhs.Storage.Shape.Dimensions[0], rhs.Storage.Shape.Dimensions[0] };
+
+            NDArray res1 = new NDArray(lhs.dtype, resultDims);
+            NDArray res2 = new NDArray(lhs.dtype, resultDims);
+
+            IArraySlice res1Arr = res1.Storage.GetData();
+            IArraySlice res2Arr = res2.Storage.GetData();
+
+            int counter = 0;
+
+            for (int row = 0; row < nd1Data.Count; row++)
+            {
+                for (int col = 0; col < nd2Data.Count; col++)
+                {
+                    res1Arr[counter] = nd1Data[row];
+                    res2Arr[counter] = nd2Data[col];
+                    counter++;
+                }
+            }
+
+
+            return (res1, res2);
+
         }
     }
 }
