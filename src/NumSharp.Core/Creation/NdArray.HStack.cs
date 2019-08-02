a0962124a54129573abe85e5d33ca3959db283c4
Eli Belash <elibelash@gmail.com>
2019-08-03 00:42:00 +0300

    Fixed documentation, added guards and perf-opped NDArray.hstack

--- src/NumSharp.Core/Creation/NdArray.HStack.cs
+++ src/NumSharp.Core/Creation/NdArray.HStack.cs
@@ -9,15 +9,21 @@ namespace NumSharp
     public partial class NDArray
     {
         /// <summary>
-        /// Stack arrays in sequence horizontally
+        ///     Stack arrays in sequence horizontally (column wise).
+        ///     This is equivalent to concatenation along the second axis, except for 1-D arrays where it concatenates along the first axis.Rebuilds arrays divided by hsplit.
+        ///     This function makes most sense for arrays with up to 3 dimensions.For instance, for pixel-data with a height(first axis), width(second axis), and r/g/b channels(third axis). The functions concatenate, stack and block provide more general stacking and concatenation operations.
         /// </summary>
-        /// <param name="nps"></param>
-        /// <returns></returns>
-        public NDArray hstack(params NDArray[] nps)
+        /// <param name="tup">The arrays must have the same shape along all but the second axis, except 1-D arrays which can be any length.</param>
+        /// <returns>The array formed by stacking the given arrays.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.hstack.html</remarks>
+        public NDArray hstack(params NDArray[] tup)
         {
-            NDArray[] list = new NDArray[1 + nps.Length];
+            if (tup == null)
+                throw new ArgumentNullException(nameof(tup));
+
+            NDArray[] list = new NDArray[1 + tup.Length];
             list[0] = this;
-            nps.CopyTo(list, 1);
+            tup.CopyTo(list, 1);
             return np.hstack(list);
         }
     }
--- src/NumSharp.Core/Creation/np.hstack.cs
+++ src/NumSharp.Core/Creation/np.hstack.cs
@@ -8,17 +8,25 @@ namespace NumSharp
     public static partial class np
     {
         /// <summary>
-        /// From https://docs.scipy.org/doc/numpy/reference/generated/numpy.hstack.html
-        /// This is equivalent to concatenation along the second axis, except for 1-D arrays where it concatenates along the first axis. 
+        ///     Stack arrays in sequence horizontally (column wise).
+        ///     This is equivalent to concatenation along the second axis, except for 1-D arrays where it concatenates along the first axis.Rebuilds arrays divided by hsplit.
+        ///     This function makes most sense for arrays with up to 3 dimensions.For instance, for pixel-data with a height(first axis), width(second axis), and r/g/b channels(third axis). The functions concatenate, stack and block provide more general stacking and concatenation operations.
         /// </summary>
-        /// <param name="nps"></param>
-        /// <returns></returns>
-        public static NDArray hstack(params NDArray[] nps)
+        /// <param name="tup">The arrays must have the same shape along all but the second axis, except 1-D arrays which can be any length.</param>
+        /// <returns>The array formed by stacking the given arrays.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.hstack.html</remarks>
+        public static NDArray hstack(params NDArray[] tup)
         {
-            if (nps[0].shape.Length == 1)
-                return np.concatenate(nps, 0);
+            if (tup == null)
+                throw new ArgumentNullException(nameof(tup));
+
+            if (tup.Length == 0)
+                throw new ArgumentException("Value cannot be an empty collection.", nameof(tup));
+
+            if (tup[0].shape.Length == 1)
+                return np.concatenate(tup, 0);
             else
-                return np.concatenate(nps, 1);
+                return np.concatenate(tup, 1);
         }
     }
 }
