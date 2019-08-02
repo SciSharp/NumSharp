e3683a41e13b25d0e8c987e9a17e65de446e42cf
Eli Belash <elibelash@gmail.com>
2019-08-02 23:43:03 +0300

    Fixed documentation, added guards and perf-opped NDArray.hstack

--- src/NumSharp.Core/Creation/NdArray.HStack.cs
+++ src/NumSharp.Core/Creation/NdArray.HStack.cs
@@ -9,16 +9,22 @@ namespace NumSharp
     public partial class NDArray
     {
         /// <summary>
-        /// Stack arrays in sequence horizontally
+        ///     Stack arrays in sequence horizontally (column wise).
+        ///     This is equivalent to concatenation along the second axis, except for 1-D arrays where it concatenates along the first axis.Rebuilds arrays divided by hsplit.
+        ///     This function makes most sense for arrays with up to 3 dimensions.For instance, for pixel-data with a height(first axis), width(second axis), and r/g/b channels(third axis). The functions concatenate, stack and block provide more general stacking and concatenation operations.
         /// </summary>
-        /// <param name="nps"></param>
-        /// <returns></returns>
-        public NDArray hstack<T>(params NDArray[] nps)
+        /// <param name="tup">The arrays must have the same shape along all but the second axis, except 1-D arrays which can be any length.</param>
+        /// <returns>The array formed by stacking the given arrays.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.hstack.html</remarks>
+        public NDArray hstack<T>(params NDArray[] tup)
         {
-            List<NDArray> list = new List<NDArray>();
-            list.Add(this);
-            list.AddRange(nps);
-            return np.hstack<T>(list.ToArray());
+            if (tup == null)
+                throw new ArgumentNullException(nameof(tup));
+
+            var nds = new NDArray[tup.Length+1];
+            nds[0] = this;
+            tup.CopyTo(nds, 1);
+            return np.hstack<T>(nds);
         }
     }
 }
