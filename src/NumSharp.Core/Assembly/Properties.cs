3429cac9d38e98d59770560e532cb2c553782bd3
Eli Belash <elibelash@gmail.com>
2019-08-20 15:26:43 +0300

    Enabled signing on release mode, upped version to 0.20.0 and updated descriptions

--- src/NumSharp.Core/Assembly/Properties.cs
+++ src/NumSharp.Core/Assembly/Properties.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
-
+#if DEBUG
 [assembly: InternalsVisibleTo("NumSharp.UnitTest")]
 [assembly: InternalsVisibleTo("NumSharp.Benchmark")]
 [assembly: InternalsVisibleTo("TensorFlowNET.UnitTest")]
+#endif
