7122e42010e0ad14e6da561c66914d5b979084e5
Eli Belash <elibelash@gmail.com>
2019-09-11 20:47:35 +0300

    NDArray.flatten(bool): fixed overload ambiguous.

--- src/NumSharp.Core/Manipulation/NDArray.flatten.cs
+++ src/NumSharp.Core/Manipulation/NDArray.flatten.cs
@@ -8,6 +8,6 @@
         /// <param name="clone">Should the data be cloned, true by default.</param>
         /// <returns>A copy of the input array, flattened to one dimension.</returns>
         /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.flatten.html</remarks>
-        public NDArray flatten(bool clone = true) => clone ? new NDArray(CloneData(), Shape.Vector(size)) : new NDArray(Storage, Shape.Vector(size));
+        public NDArray flatten(bool clone) => clone ? new NDArray(CloneData(), Shape.Vector(size)) : new NDArray(Storage, Shape.Vector(size));
     }
 }
