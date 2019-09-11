ebc7f0c80539f787b40bbd3ccd41ce172c809920
Eli Belash <elibelash@gmail.com>
2019-09-11 15:41:10 +0300

    Removed #If DEBUG from InternalsVisibleTo,

--- src/NumSharp.Core/Assembly/Properties.cs
+++ src/NumSharp.Core/Assembly/Properties.cs
@@ -1,6 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
-#if DEBUG
 [assembly: InternalsVisibleTo("NumSharp.UnitTest")]
 [assembly: InternalsVisibleTo("NumSharp.Benchmark")]
 [assembly: InternalsVisibleTo("TensorFlowNET.UnitTest")]
-#endif
