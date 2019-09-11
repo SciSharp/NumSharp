9994c2bc738f42dda0ce81bc7f8cc3add50f1e84
Eli Belash <elibelash@gmail.com>
2019-09-11 20:35:02 +0300

    NDArray: Added nd.flatten()

---
+++ src/NumSharp.Core/Creation/NDArray.flatten.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace NumSharp
+{
+    [SuppressMessage("ReSharper", "ParameterHidesMember")]
+    public partial class NDArray
+    {
+        /// <summary>
+        ///     Return a copy of the array collapsed into one dimension.
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns>A copy of the input array, flattened to one dimension.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.ndarray.flatten.html</remarks>
+        public NDArray flatten(char order = 'C') => flat.copy(order);
+    }
+}
