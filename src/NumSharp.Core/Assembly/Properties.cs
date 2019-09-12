76b58f4cc1850831e8993fdffc94e3fa0b92e996
Eli Belash <elibelash@gmail.com>
2019-09-12 16:31:31 +0300

    Added PUBLISH project configuration.

--- src/NumSharp.Core/Assembly/Properties.cs
+++ src/NumSharp.Core/Assembly/Properties.cs
@@ -1,1 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
+#if !SIGNING
+[assembly: InternalsVisibleTo("NumSharp.UnitTest")]
+[assembly: InternalsVisibleTo("NumSharp.Benchmark")]
+[assembly: InternalsVisibleTo("TensorFlowNET.UnitTest")]
+#endif
