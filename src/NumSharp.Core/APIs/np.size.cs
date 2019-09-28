6b87b9de3a18c9f329d4b9d75c216416d7ff3bc5
Eli Belash <elibelash@gmail.com>
2019-09-28 20:34:54 +0300

    Added np.size

---
+++ src/NumSharp.Core/APIs/np.size.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NumSharp
+{
+    public partial class np
+    {
+        /// <summary>
+        ///     Return the number of elements along a given axis.
+        /// </summary>
+        /// <param name="a">Input data.</param>
+        /// <param name="axis">Axis along which the elements are counted. By default, give the total number of elements.</param>
+        /// <returns>Number of elements along the specified axis.</returns>
+        /// <remarks>https://docs.scipy.org/doc/numpy/reference/generated/numpy.ma.size.html</remarks>
+        public static int size(NDArray a, int? axis = null)
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a));
+
+            if (axis == null || axis == -1)
+                return a.size;
+
+            return a.shape[axis.Value];
+        }
+
+        //based on:
+        //a=np.random.randint(0,10,(4,2,3))
+        //print(np.size(a))
+        //print(np.size(a, 0))
+        //print(np.size(a, 1))
+        //print(np.size(a, 2))
+        //print(np.size(a, -1))
+    }
+}
