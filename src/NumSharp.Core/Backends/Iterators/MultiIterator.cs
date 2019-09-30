6d69fa8409a530dcc69afda8208d5e435d61737f
Eli Belash <elibelash@gmail.com>
2019-10-01 02:38:54 +0300

    MultiIterator: Fixed case where Shapes are equal but still broadcasting them.

--- src/NumSharp.Core/Backends/Iterators/MultiIterator.cs
+++ src/NumSharp.Core/Backends/Iterators/MultiIterator.cs
@@ -220,7 +220,7 @@ namespace NumSharp
         {
             if (broadcast)
             {
-                var (leftShape, rightShape) = DefaultEngine.Broadcast(lhs.Shape, rhs.Shape);
+                var (leftShape, rightShape) = lhs.Shape == rhs.Shape ? (lhs.Shape, rhs.Shape) : DefaultEngine.Broadcast(lhs.Shape, rhs.Shape);
 
 #if _REGEN
                 #region Compute
