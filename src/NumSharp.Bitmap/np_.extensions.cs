09435e44ca4dafd3f106e9c152caf76e6988c707
Eli Belash <elibelash@gmail.com>
2019-10-06 02:43:54 +0300

    NumSharp.Bitmap: Revamped all methods for smart resolving of PixelFormat. (From and To Bitmap)
    - Added additional option: discardAlpha.
    - Automatic inference for the shape when converting NDArray to Bitmap.
    - Automatic inference for the shape when converting Bitmap to NDArray

--- src/NumSharp.Bitmap/np_.extensions.cs
+++ src/NumSharp.Bitmap/np_.extensions.cs
@@ -8,7 +8,9 @@ using NumSharp.Backends.Unmanaged;
 // ReSharper disable once CheckNamespace
 namespace NumSharp
 {
+
     [SuppressMessage("ReSharper", "SuggestVarOrType_SimpleTypes")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public static class np_
     {
         /// <summary>
@@ -17,7 +19,8 @@ namespace NumSharp
         /// <param name="image">The image to load data from.</param>
         /// <param name="flat">
         ///     If true, returns NDArray be 1-d of pixels: `R1G1B1R2G2B2 ... RnGnBn` where n is the amount of pixels in the image.<br></br>
-        ///     If false, returns a 4-d NDArray shaped: (1, bmpData.Height, bmpData.Width, 3)
+        ///     If false, returns a 4-d NDArray shaped: (1, bmpData.Height, bmpData.Width, bbp)<br></br>
+        ///         or (1, bmpData.Height, bmpData.Width, 4) if alpha is present and <paramref name="discardAlpha"/> is false.
         /// </param>
         /// <param name="copy">
         ///     If true, performs <see cref="Bitmap.LockBits(System.Drawing.Rectangle,System.Drawing.Imaging.ImageLockMode,System.Drawing.Imaging.PixelFormat)"/>
@@ -25,8 +28,10 @@ namespace NumSharp
         ///     If false, It'll call <see cref="Bitmap.LockBits(System.Drawing.Rectangle,System.Drawing.Imaging.ImageLockMode,System.Drawing.Imaging.PixelFormat)"/>
         ///     , wraps the <see cref="BitmapData.Scan0"/> with an NDArray and call <see cref="Bitmap.UnlockBits"/> only when the NDArray will be collected by the <see cref="GC"/>.
         /// </param>
+        /// <param name="discardAlpha">If the given <see cref="Bitmap"/> has an alpha pixel (transparency pixel), discard that data or return a slice without the alpha (depends on <paramref name="copy"/>).</param>
         /// <returns>An NDArray that holds the pixel data of the given bitmap</returns>
-        public static unsafe NDArray ToNDArray(this System.Drawing.Bitmap image, bool flat = false, bool copy = true)
+        [SuppressMessage("ReSharper", "SuggestVarOrType_Elsewhere")]
+        public static unsafe NDArray ToNDArray(this System.Drawing.Bitmap image, bool flat = false, bool copy = true, bool discardAlpha = false)
         {
             if (image == null) throw new ArgumentNullException(nameof(image));
 
@@ -44,8 +49,12 @@ namespace NumSharp
                         byte* dst = (byte*)nd.Unsafe.Address; //we can use unsafe because we just allocated that array and we know for sure it is contagious.
 
                         // Copy the RGB values into the array.
-                        Buffer.MemoryCopy(src, dst, nd.size, nd.size); //faster than Marshal.Copy
-                        return nd.reshape(1, image.Height, image.Width, 3);
+                        Buffer.MemoryCopy(src, dst, bmpData.Stride * image.Height, nd.size); //faster than Marshal.Copy
+                        var ret = nd.reshape(1, image.Height, image.Width, bmpData.Stride / bmpData.Width);
+                        if (discardAlpha && ret.shape[3] == 4)
+                            ret = ret[Slice.All, Slice.All, Slice.All, new Slice(stop: 3)];
+
+                        return flat && ret.ndim != 1 ? ret.flat : ret;
                     }
                 }
                 finally
@@ -54,8 +63,46 @@ namespace NumSharp
                 }
             else
             {
-                var nd = new NDArray(new ArraySlice<byte>(new UnmanagedMemoryBlock<byte>((byte*)bmpData.Scan0, bmpData.Stride * bmpData.Height, () => image.UnlockBits(bmpData))));
-                return flat ? nd : nd.reshape(1, bmpData.Height, bmpData.Width, 3);
+                var ret = new NDArray(new ArraySlice<byte>(new UnmanagedMemoryBlock<byte>((byte*)bmpData.Scan0, bmpData.Stride * bmpData.Height, () => image.UnlockBits(bmpData))));
+
+                if (flat)
+                {
+                    if (discardAlpha)
+                    {
+                        if (bmpData.Stride / bmpData.Width == 4) //1byte-per-color
+                        {
+                            return ret.reshape(1, bmpData.Height, bmpData.Width, bmpData.Stride / bmpData.Width) //reshape
+                                [Slice.All, Slice.All, Slice.All, new Slice(stop: 3)] //slice
+                                .flat; //flatten
+                        }
+
+                        if (bmpData.Stride / bmpData.Width == 8) //2bytes-per-color
+                        {
+                            return ret.reshape(1, bmpData.Height, bmpData.Width, bmpData.Stride / bmpData.Width) //reshape
+                                [Slice.All, Slice.All, Slice.All, new Slice(stop: 6)] //slice
+                                .flat; //flatten
+                        }
+
+                        throw new NotSupportedException($"Given bbp ({bmpData.Stride / bmpData.Width}) is not supported.");
+                    }
+
+                    return ret.flat;
+                }
+                else
+                {
+                    ret = ret.reshape(1, bmpData.Height, bmpData.Width, bmpData.Stride / bmpData.Width); //reshape
+                    if (discardAlpha)
+                    {
+                        if (ret.shape[3] == 4) //1byte-per-color
+                            ret = ret[Slice.All, Slice.All, Slice.All, new Slice(stop: 3)]; //slice
+                        else if (ret.shape[3] == 8) //2bytes-per-color
+                            ret = ret[Slice.All, Slice.All, Slice.All, new Slice(stop: 6)]; //slice
+                        else
+                            throw new NotSupportedException($"Given bbp ({bmpData.Stride / bmpData.Width}) is not supported.");
+                    }
+
+                    return ret;
+                }
             }
         }
 
@@ -65,7 +112,7 @@ namespace NumSharp
         /// <param name="image">The image to load data from.</param>
         /// <param name="flat">
         ///     If true, returns NDArray be 1-d of pixels: `R1G1B1R2G2B2 ... RnGnBn` where n is the amount of pixels in the image.<br></br>
-        ///     If false, returns a 4-d NDArray shaped: (1, bmpData.Height, bmpData.Width, 3)
+        ///     If false, returns a 4-d NDArray shaped: (1, bmpData.Height, bmpData.Width, bbp)
         /// </param>
         /// <param name="copy">
         ///     If true, performs <see cref="Bitmap.LockBits(System.Drawing.Rectangle,System.Drawing.Imaging.ImageLockMode,System.Drawing.Imaging.PixelFormat)"/>
@@ -73,13 +120,14 @@ namespace NumSharp
         ///     If false, It'll call <see cref="Bitmap.LockBits(System.Drawing.Rectangle,System.Drawing.Imaging.ImageLockMode,System.Drawing.Imaging.PixelFormat)"/>
         ///     , wraps the <see cref="BitmapData.Scan0"/> with an NDArray and call <see cref="Bitmap.UnlockBits"/> only when the NDArray will be collected by the <see cref="GC"/>.
         /// </param>
+        /// <param name="discardAlpha">If the given <see cref="Bitmap"/> has an alpha pixel (transparency pixel), discard that data or return a slice without the alpha (depends on <paramref name="copy"/>).</param>
         /// <returns>An NDArray that holds the pixel data of the given bitmap</returns>
-        public static NDArray ToNDArray(this Image image, bool flat = false, bool copy = true)
+        public static NDArray ToNDArray(this Image image, bool flat = false, bool copy = true, bool discardAlpha = false)
         {
             if (image == null)
                 throw new ArgumentNullException(nameof(image));
 
-            return ToNDArray(new System.Drawing.Bitmap(image), flat, copy);
+            return ToNDArray(new System.Drawing.Bitmap(image), flat, copy, discardAlpha);
         }
 
         /// <summary>
@@ -88,17 +136,37 @@ namespace NumSharp
         /// <param name="bmpData">Targetted bitmap data with reading capabilities.</param>
         /// <param name="flat">
         ///     If true, returns NDArray be 1-d of pixels: R1G1B1R2G2B2 ... RnGnBn where n is the amount of pixels in the image.<br></br>
-        ///     If false, returns a 4-d NDArray shaped: (1, bmpData.Height, bmpData.Width, 3)
+        ///     If false, returns a 4-d NDArray shaped: (1, bmpData.Height, bmpData.Width, bbp)
         /// </param>
+        /// <param name="discardAlpha">If the given <see cref="Bitmap"/> has an alpha pixel (transparency pixel), return a slice without the alpha.</param>
         /// <returns>An NDArray that wraps the given bitmap, doesn't copy</returns>
         /// <remarks>If the BitmapData is unlocked via <see cref="Bitmap.UnlockBits"/> - the NDArray will point to an invalid address which will cause heap corruption. Use with caution!</remarks>
-        public static unsafe NDArray AsNDArray(this BitmapData bmpData, bool flat = false)
+        public static unsafe NDArray AsNDArray(this BitmapData bmpData, bool flat = false, bool discardAlpha = false)
         {
             if (bmpData == null)
                 throw new ArgumentNullException(nameof(bmpData));
 
-            var nd = new NDArray(new ArraySlice<byte>(new UnmanagedMemoryBlock<byte>((byte*)bmpData.Scan0, bmpData.Stride * bmpData.Height)));
-            return flat ? nd : nd.reshape(1, bmpData.Height, bmpData.Width, 3);
+            var ret = new NDArray(new ArraySlice<byte>(new UnmanagedMemoryBlock<byte>((byte*)bmpData.Scan0, bmpData.Stride * bmpData.Height)));
+            if (flat)
+            {
+                if (ret.shape[3] == 4 && discardAlpha)
+                {
+                    return ret.reshape(1, bmpData.Height, bmpData.Width, bmpData.Stride / bmpData.Width) //reshape
+                        [Slice.All, Slice.All, Slice.All, new Slice(stop: 3)] //slice
+                        .flat; //flatten
+                }
+
+                return ret;
+            }
+            else
+            {
+                ret = ret.reshape(1, bmpData.Height, bmpData.Width, bmpData.Stride / bmpData.Width); //reshape
+
+                if (ret.shape[3] == 4 && discardAlpha)
+                    ret = ret[Slice.All, Slice.All, Slice.All, new Slice(stop: 3)]; //slice
+
+                return ret;
+            }
         }
 
         /// <summary>
@@ -107,24 +175,42 @@ namespace NumSharp
         /// <param name="nd">The <see cref="NDArray"/> to copy pixels from, <see cref="Shape"/> is ignored completely. If nd.Unsafe.Shape.IsContiguous == false then a copy is made.</param>
         /// <param name="width">The height of the <see cref="Bitmap"/></param>
         /// <param name="height">The width of the <see cref="Bitmap"/></param>
+        /// <param name="format">The format of the expected bitmap, Must be matching to given NDArray otherwise unexpected results might occur.</param>
         /// <returns>A <see cref="Bitmap"/></returns>
         /// <exception cref="ArgumentException">When nd.size != width*height, which means the ndarray be turned into the given bitmap size.</exception>
-        public static unsafe Bitmap ToBitmap(this NDArray nd, int width, int height)
+        public static unsafe Bitmap ToBitmap(this NDArray nd, int width, int height, PixelFormat format = PixelFormat.DontCare)
         {
             if (nd == null)
                 throw new ArgumentNullException(nameof(nd));
 
-            if (width * height != nd.size)
-                throw new ArgumentException("Given nd.size != width*height");
+            //if flat then initialize based on given format
+            if (nd.ndim == 1 && format != PixelFormat.DontCare)
+                nd = nd.reshape(1, height, width, format.ToBytesPerPixel()); //theres a check internally for size mismatch.
 
-            if (!nd.Shape.IsContiguous)
-                nd = nd.Clone();
+            if (nd.ndim != 4)
+                throw new ArgumentException("ndarray was expected to be of 4-dimensions, (1, bmpData.Height, bmpData.Width, bytesPerPixel)");
 
-            var ret = new Bitmap(height, width);
-            var bitdata = ret.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+            if (nd.shape[0] != 1)
+                throw new ArgumentException($"ndarray has more than one picture in it ({nd.shape[0]}) based on the first dimension.");
+
+
+            var bbp = nd.shape[3]; //bytes per pixel.
+            if (bbp != extractFormatNumber())
+                throw new ArgumentException($"Given PixelFormat: {format} does not match the number of bytes per pixel in the 4th dimension of given ndarray.");
+
+            if (bbp * width * height != nd.size)
+                throw new ArgumentException($"The expected size does not match the size of given ndarray. (expected: {bbp * width * height}, actual: {nd.size})");
+
+
+            var ret = new Bitmap(width, height, format);
+            var bitdata = ret.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, format);
             try
             {
-                nd.CopyTo(new UnmanagedMemoryBlock<byte>((byte*)bitdata.Scan0, bitdata.Stride * bitdata.Height));
+                var dst = new ArraySlice<byte>(new UnmanagedMemoryBlock<byte>((byte*)bitdata.Scan0, bitdata.Stride * bitdata.Height));
+                if (nd.Shape.IsContiguous)
+                    nd.CopyTo(dst);
+                else
+                    MultiIterator.Assign(new UnmanagedStorage(dst, Shape.Vector(bitdata.Stride * bitdata.Height)), nd.Unsafe.Storage);
             }
             finally
             {
@@ -132,6 +218,117 @@ namespace NumSharp
             }
 
             return ret;
+
+            int extractFormatNumber()
+            {
+                if (format == PixelFormat.DontCare)
+                {
+                    switch (bbp)
+                    {
+                        case 3:
+                            format = PixelFormat.Format24bppRgb;
+                            break;
+                        case 4:
+                            format = PixelFormat.Format32bppArgb;
+                            break;
+                        case 6:
+                            format = PixelFormat.Format48bppRgb;
+                            break;
+                        case 8:
+                            format = PixelFormat.Format64bppArgb;
+                            break;
+                    }
+
+                    return bbp;
+                }
+
+                return format.ToBytesPerPixel();
+            }
+        }
+
+        /// <summary>
+        ///     Converts <see cref="NDArray"/> to a <see cref="Bitmap"/>. 
+        /// </summary>
+        /// <param name="nd">The <see cref="NDArray"/> to copy pixels from, <see cref="Shape"/> is ignored completely. If nd.Unsafe.Shape.IsContiguous == false then a copy is made.</param>
+        /// <param name="format">The format of the expected bitmap, Must be matching to given NDArray otherwise unexpected results might occur.</param>
+        /// <returns>A <see cref="Bitmap"/></returns>
+        /// <exception cref="ArgumentException">When nd.size != width*height, which means the ndarray be turned into the given bitmap size.</exception>
+        public static Bitmap ToBitmap(this NDArray nd, PixelFormat format = PixelFormat.DontCare)
+        {
+            if (nd == null)
+                throw new ArgumentNullException(nameof(nd));
+            if (nd.ndim != 4)
+                throw new ArgumentException("ndarray was expected to be of 4-dimensions, (1, bmpData.Height, bmpData.Width, bytesPerPixel)");
+            if (nd.shape[0] != 1)
+                throw new ArgumentException($"ndarray has more than one picture in it ({nd.shape[0]}) based on the first dimension.");
+
+            var height = nd.shape[1];
+            var width = nd.shape[2];
+
+            return ToBitmap(nd, width, height, format);
+        }
+
+        /// <summary>
+        ///     Returns how many bytes are per pixel based on given <paramref name="format"/>.
+        /// </summary>
+        /// <exception cref="ArgumentException">The format is not supported by us.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Invalid PixelFormat enum value.</exception>
+        public static int ToBytesPerPixel(this PixelFormat format)
+        {
+            int ret;
+            switch (format)
+            {
+                case PixelFormat.Format16bppArgb1555:
+                    ret = 16;
+                    break;
+                case PixelFormat.Format16bppGrayScale:
+                    ret = 16;
+                    break;
+                case PixelFormat.Format16bppRgb555:
+                    ret = 16;
+                    break;
+                case PixelFormat.Format16bppRgb565:
+                    ret = 16;
+                    break;
+                case PixelFormat.Format24bppRgb:
+                    ret = 24;
+                    break;
+                case PixelFormat.Format32bppArgb:
+                    ret = 32;
+                    break;
+                case PixelFormat.Format32bppPArgb:
+                    ret = 32;
+                    break;
+                case PixelFormat.Format32bppRgb:
+                    ret = 32;
+                    break;
+                case PixelFormat.Format48bppRgb:
+                    ret = 48;
+                    break;
+                case PixelFormat.Format64bppArgb:
+                    ret = 64;
+                    break;
+                case PixelFormat.Format64bppPArgb:
+                    ret = 64;
+                    break;
+                case PixelFormat.Format1bppIndexed:
+                case PixelFormat.Format8bppIndexed:
+                case PixelFormat.Format4bppIndexed:
+                case PixelFormat.Alpha:
+                case PixelFormat.Canonical:
+                case PixelFormat.Extended:
+                case PixelFormat.Gdi:
+                case PixelFormat.Indexed:
+                case PixelFormat.Max:
+                case PixelFormat.PAlpha:
+                    throw new ArgumentException($"Given PixelFormat: {format} is not supported.");
+                case PixelFormat.DontCare:
+                    throw new ArgumentException("Given PixelFormat can't be DontCare.");
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(format), format, null);
+            }
+
+            return ret / 8;
         }
     }
 }
---
+++ test/NumSharp.UnitTest/NumSharp.Bitmap/BitmapWithAlphaTests.cs
@@ -0,0 +1,90 @@
+﻿using System.Drawing.Imaging;
+using System.Resources;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumSharp.UnitTest.Utilities;
+
+namespace NumSharp.UnitTest
+{
+    [TestClass]
+    public class BitmapWithAlphaTests : TestClass
+    {
+        [TestMethod]
+        public void ToNDArray_Case1()
+        {
+            var bitmap = EmbeddedBitmap("captcha-a");
+            var nd = bitmap.ToNDArray(discardAlpha: true, copy: true);
+            nd.Should().BeShaped(1, 165, 400, 3);
+        }
+
+        [TestMethod]
+        public void ToNDArray_Case2()
+        {
+            var bitmap = EmbeddedBitmap("captcha-a");
+            var nd = bitmap.ToNDArray(discardAlpha: true, copy: false);
+            nd.Should().BeShaped(1, 165, 400, 3);
+        }
+
+        [TestMethod]
+        public void ToNDArray_Case3()
+        {
+            var bitmap = EmbeddedBitmap("captcha-a");
+            var nd = bitmap.ToNDArray(discardAlpha: false, copy: true);
+            nd.Should().BeShaped(1, 165, 400, 4);
+        }
+
+        [TestMethod]
+        public void ToNDArray_Case4()
+        {
+            var bitmap = EmbeddedBitmap("captcha-a");
+            var nd = bitmap.ToNDArray(discardAlpha: false, copy: false);
+            nd.Should().BeShaped(1, 165, 400, 4);
+        }
+
+        [TestMethod]
+        public void ToNDArray_Case5_flat()
+        {
+            var bitmap = EmbeddedBitmap("captcha-a");
+            var nd = bitmap.ToNDArray(discardAlpha: true, copy: true, flat: true);
+            nd.Should().BeShaped(1 * 165 * 400 * 3);
+        }
+
+        [TestMethod]
+        public void ToNDArray_Case6_flat()
+        {
+            var bitmap = EmbeddedBitmap("captcha-a");
+            var nd = bitmap.ToNDArray(discardAlpha: true, copy: false, flat: true);
+            nd.Should().BeShaped(1 * 165 * 400 * 3);
+        }
+
+        [TestMethod]
+        public void ToNDArray_Case7_flat()
+        {
+            var bitmap = EmbeddedBitmap("captcha-a");
+            var nd = bitmap.ToNDArray(discardAlpha: false, copy: true, flat: true);
+            nd.Should().BeShaped(1 * 165 * 400 * 4);
+        }
+
+        [TestMethod]
+        public void ToNDArray_Case8_flat()
+        {
+            var bitmap = EmbeddedBitmap("captcha-a");
+            var nd = bitmap.ToNDArray(discardAlpha: false, copy: false, flat: true);
+            nd.Should().BeShaped(1 * 165 * 400 * 4);
+        }
+
+        [TestMethod]
+        public void ToBitmap_PixelFormat_DontCare_Case1()
+        {
+            var bm = np.arange(0, 3 * 3 * 3).reshape(1, 3, 3, 3).astype(NPTypeCode.Byte).ToBitmap();
+            bm.PixelFormat.Should().Be(PixelFormat.Format24bppRgb);
+        }
+
+        [TestMethod]
+        public void ToBitmap_PixelFormat_DontCare_Case2()
+        {
+            var bm = np.arange(0, 3 * 3 * 4).reshape(1, 3, 3, 4).astype(NPTypeCode.Byte).ToBitmap();
+            bm.PixelFormat.Should().Be(PixelFormat.Format32bppArgb);
+        }
+    }
+}
