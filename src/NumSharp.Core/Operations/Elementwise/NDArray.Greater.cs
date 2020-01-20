00d8700b00e815f321238536e0d6b4dbc9af8d6a
Eli Belash <elibelash@gmail.com>
2020-01-21 00:21:29 +0200

    Revert "Operator > and < for int and float #362"
    This reverts commit 3a2792ce6fa0b708dd464a4567d8ea12592688c9.

--- src/NumSharp.Core/Operations/Elementwise/NDArray.Greater.cs
+++ src/NumSharp.Core/Operations/Elementwise/NDArray.Greater.cs
@@ -1,69 +1,7 @@
-﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
-using NumSharp.Backends;
-using NumSharp.Generic;
-using NumSharp.Utilities;
-
-namespace NumSharp
+﻿namespace NumSharp
 {
     public partial class NDArray
     {
-        [MethodImpl((MethodImplOptions)768)]
-        [SuppressMessage("ReSharper", "JoinDeclarationAndInitializer")]
-        [SuppressMessage("ReSharper", "CompareOfFloatsByEqualityOperator")]
-        public static unsafe NumSharp.Generic.NDArray<bool> operator >(NDArray lhs, NDArray rhs)
-        {
-            if(lhs.typecode == NPTypeCode.Int32 && rhs.typecode == NPTypeCode.Int32)
-            {
-                if (lhs.Shape.IsScalar && rhs.Shape.IsScalar)
-                    return NDArray.Scalar<bool>(*(int*)lhs.Address > *(int*)rhs.Address).MakeGeneric<bool>();
-
-                (Shape BroadcastedLeftShape, Shape BroadcastedRightShape) = DefaultEngine.Broadcast(lhs.Shape, rhs.Shape);
-                var lhs_address = (int*)lhs.Address;
-                var rhs_address = (int*)rhs.Address;
-                var ret = new NDArray<bool>(new Shape(BroadcastedLeftShape.dimensions), true);
-                Shape retShape = ret.Shape;
-
-                //iterate
-                var ret_address = (bool*)ret.Address;
-                var incr = new NDCoordinatesIncrementor(BroadcastedLeftShape.dimensions); //doesn't matter which side it is.
-                int[] current = incr.Index;
-                do
-                {
-                    *(ret_address + retShape.GetOffset(current)) = (*(lhs_address + BroadcastedLeftShape.GetOffset(current))) > *(rhs_address + BroadcastedRightShape.GetOffset(current));
-                } while (incr.Next() != null);
-
-                return ret;
-            }
-            else if (lhs.typecode == NPTypeCode.Float && rhs.typecode == NPTypeCode.Float)
-            {
-                if (lhs.Shape.IsScalar && rhs.Shape.IsScalar)
-                    return NDArray.Scalar<bool>(*(float*)lhs.Address > *(float*)rhs.Address).MakeGeneric<bool>();
-
-                (Shape BroadcastedLeftShape, Shape BroadcastedRightShape) = DefaultEngine.Broadcast(lhs.Shape, rhs.Shape);
-                var lhs_address = (float*)lhs.Address;
-                var rhs_address = (float*)rhs.Address;
-                var ret = new NDArray<bool>(new Shape(BroadcastedLeftShape.dimensions), true);
-                Shape retShape = ret.Shape;
-
-                //iterate
-                var ret_address = (bool*)ret.Address;
-                var incr = new NDCoordinatesIncrementor(BroadcastedLeftShape.dimensions); //doesn't matter which side it is.
-                int[] current = incr.Index;
-                do
-                {
-                    *(ret_address + retShape.GetOffset(current)) = (*(lhs_address + BroadcastedLeftShape.GetOffset(current))) > *(rhs_address + BroadcastedRightShape.GetOffset(current));
-                } while (incr.Next() != null);
-
-                return ret;
-            }
-            else
-            {
-                throw new NotImplementedException($"{lhs.typecode} > {rhs.typecode}");
-            }
-        }
-
         public static NumSharp.Generic.NDArray<bool> operator >(NDArray np, int obj)
         {
             return (np > (System.Object)obj);
