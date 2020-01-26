f8ad4cca48dc2309b0092aa5a6250f5e27ea6abe
Haiping <haiping008@gmail.com>
2020-01-26 15:39:31 -0600

    support greater and less for same data type. (#393)

---
+++ test/NumSharp.UnitTest/Operations/NDArray.GRATER.Test.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using NumSharp;
+using NumSharp.Generic;
+
+namespace NumSharp.UnitTest.Operations
+{
+    [TestClass]
+    public class NDArrayGreaterOrLessTest
+    {
+        [TestMethod]
+        public void BoolTwoScalar_NDArray()
+        {
+            NDArray np1 = 1;
+            NDArray np2 = 2;
+            bool np3 = np1 > np2;
+            Assert.IsTrue(!np3);
+
+            np1 = 3;
+            np2 = 2;
+            np3 = np1 > np2;
+            Assert.IsTrue(np3);
+
+            np3 = np2 < np1;
+            Assert.IsTrue(np3);
+        }
+
+        [TestMethod]
+        public void BoolTwo1D_NDArray()
+        {
+            var np1 = new NDArray(new[] { 1, 2, 3, 4 }, new Shape(4));
+            var np2 = new NDArray(new[] { 4, 3, 2, 1 }, new Shape(4));
+
+            var np3 = np1 > np2;
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { false, false, true, true }, np3.Data<bool>()));
+
+            np3 = np2 < np1;
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { false, false, true, true }, np3.Data<bool>()));
+        }
+    }
+}
