49404e5a95df751c2a9ba31a97f4f1c60ef87e0c
Daerhiel <485120+daerhiel@users.noreply.github.com>
2020-07-29 09:33:41 +0300

    Make type inference an operator based

--- src/NumSharp.Core/Operations/Elementwise/NDArray.Core.cs
+++ src/NumSharp.Core/Operations/Elementwise/NDArray.Core.cs
@@ -10,12 +10,13 @@ namespace NumSharp
     {
         protected static BinaryOperationIndex OpBinary = new BinaryOperationIndex(typeof(NDArray), nameof(BinaryOperation));
         protected static BinaryOperationIndex OpBinaryLeft = new BinaryOperationIndex(typeof(NDArray), nameof(BinaryOperationLeft));
+        protected static BinaryOperationIndex OpBinaryRight = new BinaryOperationIndex(typeof(NDArray), nameof(BinaryOperationRight));
 
         /// <summary>
         /// Aplies the (x, y) => z projection delegate to each element of <see cref="NDArray"/> and converts it to <typeparamref name="TResult"/>.
         /// </summary>
         /// <typeparam name="TLElement">The type of a left <see cref="NDArray"/> element.</typeparam>
-        /// <typeparam name="TRElement">The type of a right <see cref="NDArray"/> element.</typeparam>
+        /// <typeparam name="TRElement">The type of a right scalar element.</typeparam>
         /// <param name="lhs">The left <see cref="NDArray"/> to pass to the projector.</param>
         /// <param name="rhs">The right <typeparamref name="TRElement"/> scalar value to pass to the projector.</param>
         /// <param name="projector">The projection delegate that handles the array elements.</param>
@@ -33,15 +34,44 @@ namespace NumSharp
             if (typeof(TLElement).GetTypeCode() != lhs.GetTypeCode)
                 throw new ArgumentException($"The left argument array type {lhs.GetTypeCode} does not match the expected type {typeof(TLElement).GetTypeCode()}.", nameof(lhs));
 
-            var result = new NDArray(np._FindCommonArrayScalarType(lhs.GetTypeCode, typeof(TRElement).GetTypeCode()), lhs.Shape.Clean());
-
-            if (typeof(TResult).GetTypeCode() != result.GetTypeCode)
-                throw new InvalidOperationException($"The resulting array type {lhs.GetTypeCode} does not match the expected type {typeof(TResult).GetTypeCode()}.");
+            var result = new NDArray(typeof(TResult).GetTypeCode(), lhs.Shape.Clean());
 
             var lhsAddress = (TLElement*)lhs.Address;
             var resultAddress = (TResult*)result.Address;
 
             @operator.ParallelFor(0, result.size, resultAddress, lhsAddress, rhs);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Aplies the (x, y) => z projection delegate to each element of <see cref="NDArray"/> and converts it to <typeparamref name="TResult"/>.
+        /// </summary>
+        /// <typeparam name="TLElement">The type of a left scalar element.</typeparam>
+        /// <typeparam name="TRElement">The type of a right <see cref="NDArray"/> element.</typeparam>
+        /// <param name="lhs">The right <typeparamref name="TRElement"/> scalar value to pass to the projector.</param>
+        /// <param name="rhs">The left <see cref="NDArray"/> to pass to the projector.</param>
+        /// <param name="projector">The projection delegate that handles the array elements.</param>
+        /// <returns>The resulting <see cref="NDArray{TDType}"/> that contains the projection result.</returns>
+        private static unsafe NDArray BinaryOperationRight<TLElement, TRElement, TResult>(TLElement lhs, NDArray rhs, BinaryOperator<TLElement, TRElement, TResult> @operator)
+            where TLElement : unmanaged
+            where TRElement : unmanaged
+            where TResult : unmanaged
+        {
+            if (rhs is null)
+                throw new ArgumentNullException(nameof(rhs));
+            if (@operator is null)
+                throw new ArgumentNullException(nameof(@operator));
+
+            if (typeof(TLElement).GetTypeCode() != rhs.GetTypeCode)
+                throw new ArgumentException($"The right argument array type {rhs.GetTypeCode} does not match the expected type {typeof(TRElement).GetTypeCode()}.", nameof(lhs));
+
+            var result = new NDArray(typeof(TResult).GetTypeCode(), rhs.Shape.Clean());
+
+            var rhsAddress = (TRElement*)rhs.Address;
+            var resultAddress = (TResult*)result.Address;
+
+            @operator.ParallelFor(0, result.size, resultAddress, lhs, rhsAddress);
 
             return result;
         }
@@ -75,10 +105,7 @@ namespace NumSharp
 
             var (ls, rs) = DefaultEngine.Broadcast(lhs.Shape, rhs.Shape);
             var resultShape = ls.Clean();
-            var result = new NDArray(np._FindCommonType(lhs, rhs), resultShape);
-
-            if (typeof(TResult).GetTypeCode() != result.GetTypeCode)
-                throw new InvalidOperationException($"The resulting array type {result.GetTypeCode} does not match the expected type {typeof(TResult).GetTypeCode()}.");
+            var result = new NDArray(typeof(TResult).GetTypeCode(), resultShape);
 
             var lhsAddress = (TLElement*)lhs.Address;
             var rhsAddress = (TRElement*)rhs.Address;
