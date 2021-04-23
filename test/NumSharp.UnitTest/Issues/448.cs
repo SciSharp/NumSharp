32b9c086aafbfdaef8015aad81436a238adbe396
Eli Belash <elibelash@gmail.com>
2021-04-23 15:33:22 +0300

    Testcase for issue #448

---
+++ test/NumSharp.UnitTest/Issues/448.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions.Execution;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumSharp.UnitTest.Utilities;
+
+namespace NumSharp.UnitTest.Issues
+{
+    [TestClass]
+    public class Issue448
+    {
+        [TestMethod]
+        public void ReproducingTest1()
+        {
+            NDArray nd = new double[,] {{1, 2, 3}, {4, 5, 6}};
+            (nd < 3).Should().BeOfValues(true, true, false, false, false, false);
+
+            nd[(NDArray)(nd < 3)] = -2;
+            nd.Should().BeOfValues(-2, -2, 3, 4, 5, 6);
+        }
+
+        [TestMethod]
+        public void ReproducingTest2()
+        {
+            NDArray nd = new double[,] {{1, 2, 3}, {4, 5, 6}};
+            (nd < 3).Should().BeOfValues(true, true, false, false, false, false);
+
+            nd[(NDArray)(nd < 3)] = -2;
+            var values = new int[] {-2, -2, 3, 4, 5, 6};
+
+            var iter = nd.AsIterator<double>();
+            var next = iter.MoveNext;
+            var hasnext = iter.HasNext;
+            for (int i = 0; i < values.Length; i++)
+            {
+                next();
+            }
+        }
+    }
+}
