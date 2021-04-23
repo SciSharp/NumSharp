4eca228e9ebbbc17a15e8080e055175d3e969703
Eli Belash <elibelash@gmail.com>
2021-04-23 15:24:48 +0300

    Testcase for issue #447

---
+++ test/NumSharp.UnitTest/Issues/447.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumSharp.UnitTest.Issues
+{
+    [TestClass]
+    public class Issue447
+    {
+        /// <summary>
+        ///     https://github.com/SciSharp/NumSharp/issues/447#issuecomment-825556230
+        /// </summary>
+        [TestMethod]
+        public void ReproducingTest()
+        {
+            NDArray array3d = new NDArray(typeof(double), new Shape(new int[] {10, 50, 50}), true);
+            var sumOverAxis0 = np.sum(array3d, axis: 0);
+        }
+    }
+}
