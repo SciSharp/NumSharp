fce3f153c3b406e5114b21679a111951c3a1e8c9
Eli Belash <elibelash@gmail.com>
2021-04-23 15:24:57 +0300

    Testcase for issue #443

---
+++ test/NumSharp.UnitTest/Issues/443.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NumSharp.UnitTest.Issues
+{
+    [TestClass]
+    public class Issue443
+    {
+        /// <summary>
+        ///     https://github.com/SciSharp/NumSharp/issues/443#issue-825238582
+        /// </summary>
+        [TestMethod]
+        public void ReproducingTest()
+        {
+            var ones = np.ones((10, 1));
+            ones.negate();
+        }
+    }
+}
