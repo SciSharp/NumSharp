11a7d8870b541f8df68c52d3cdafb7779a9809fb
Thales Magalhães <thls.mglhs@gmail.com>
2024-09-24 11:34:41 -0300

    Add more tests for `np.load`

--- test/NumSharp.UnitTest/APIs/np.load.Test.cs
+++ test/NumSharp.UnitTest/APIs/np.load.Test.cs
@@ -20,16 +20,6 @@ namespace NumSharp.UnitTest.APIs
         }
 
         [TestMethod]
-        public void NumpyLoad1DimTest()
-        {
-            int[] arr = np.Load<int[]>(@"data/1-dim-int32_4_comma_empty.npy");
-            Assert.IsTrue(arr[0] == 0);
-            Assert.IsTrue(arr[1] == 1);
-            Assert.IsTrue(arr[2] == 2);
-            Assert.IsTrue(arr[3] == 3);
-        }
-
-        [TestMethod]
         public void NumpyNPZRoundTripTest()
         {
             int[] arr = np.Load<int[]>(@"data/1-dim-int32_4_comma_empty.npy");
@@ -41,6 +31,81 @@ namespace NumSharp.UnitTest.APIs
             ms.Position = 0L;
             var d2 = np.Load_Npz<Array>(ms);
             Assert.IsTrue(d2.Count == 2);
+        }
+
+        [TestMethod]
+        // [DataRow(@"data/arange_f2_le.npy")] // Ignore: Half-precision floats not supported
+        [DataRow(@"data/arange_f4_le.npy")]
+        [DataRow(@"data/arange_f8_le.npy")]
+        [DataRow(@"data/arange_i1.npy")]
+        [DataRow(@"data/arange_i2_le.npy")]
+        [DataRow(@"data/arange_i4_le.npy")]
+        [DataRow(@"data/arange_i8_le.npy")]
+        [DataRow(@"data/arange_u1.npy")]
+        [DataRow(@"data/arange_u2_le.npy")]
+        [DataRow(@"data/arange_u4_le.npy")]
+        [DataRow(@"data/arange_u8_le.npy")]
+        // [DataRow(@"data/arange_f2_be.npy")] // Ignore: Big-endian types not supported
+        // [DataRow(@"data/arange_f4_be.npy")] // Ignore: Big-endian types not supported
+        // [DataRow(@"data/arange_f8_be.npy")] // Ignore: Big-endian types not supported
+        // [DataRow(@"data/arange_i2_be.npy")] // Ignore: Big-endian types not supported
+        // [DataRow(@"data/arange_i4_be.npy")] // Ignore: Big-endian types not supported
+        // [DataRow(@"data/arange_i8_be.npy")] // Ignore: Big-endian types not supported
+        // [DataRow(@"data/arange_u2_be.npy")] // Ignore: Big-endian types not supported
+        // [DataRow(@"data/arange_u4_be.npy")] // Ignore: Big-endian types not supported
+        // [DataRow(@"data/arange_u8_be.npy")] // Ignore: Big-endian types not supported
+        public void load_Arange(string path)
+        {
+            NDArray arr = np.load(path);            
+            // Assert.IsNotNull(arr);
+            // Assert.IsTrue(arr.ndim == 1 && arr.shape[0] > 0);
+
+            for (int i = 0; i < arr.shape[0]; ++i)
+            {
+                int value = (int)Convert.ChangeType(arr.GetValue(i), typeof(int));
+                Assert.AreEqual(i, value);
+            }
+        }
+
+        [TestMethod]
+        [DataRow(@"data/hello_S5.npy")]
+        // [DataRow(@"data/hello_U5_be.npy")] // Ignore: Unicode strings not supported
+        // [DataRow(@"data/hello_U5_le.npy")] // Ignore: Unicode strings not supported
+        public void load_HelloWorld(string path)
+        {
+            string[] arr = np.Load<string[]>(path);
+
+            Assert.AreEqual("Hello", arr[0]);
+            Assert.AreEqual("World", arr[1]);
+        }
+
+        [TestMethod]
+        [DataRow(@"data/mgrid_i4.npy")]
+        // [DataRow(@"data/mgrid_i4_fortran_order.npy")] // Ignore: Fortran order not supported
+        public void load_Mgrid(string path)
+        {
+            NDArray arr = np.load(path);
+
+            for (int i = 0; i < arr.shape[0]; i++)
+            {
+                for (int j = 0; j < arr.shape[1]; j++)
+                {
+                    Assert.AreEqual(i, (int)arr.GetValue(0, i, j));
+                    Assert.AreEqual(j, (int)arr.GetValue(1, i, j));
+                }
+            }
+        }
+
+        [TestMethod]
+        [DataRow(@"data/scalar_b1.npy", false)]
+        [DataRow(@"data/scalar_i4_le.npy", 42)]
+        // [DataRow(@"data/scalar_i4_be.npy", 42)] // Ignore: Big-endian types not supported
+        public void load_Scalar(string path, object expected)
+        {
+            NDArray arr = np.load(path);
+            
+            Assert.AreEqual(Shape.Scalar, arr.shape);
+            Assert.AreEqual(expected, arr.GetValue(0));
         }
     }
 }
