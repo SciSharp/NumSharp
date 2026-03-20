336f9d8d79a8a92202ca7f8e1429ea3f9f819306
Eli Belash <elibelash@gmail.com>
2026-03-20 13:27:25 +0200

    feat: implement np.split and np.array_split
    - np.split: splits array into equal sub-arrays (raises ArgumentException if not divisible)
    - np.array_split: splits array into sub-arrays allowing unequal division
    - Supports int (number of sections) and int[] (split indices)
    - Supports axis parameter for multi-dimensional arrays
    - Follows NumPy's swap-slice-swap approach for axis handling
    - Returns views (not copies) matching NumPy behavior
    - Full test coverage with NumPy 2.4.2 verified behavior
    
    NumPy reference: numpy/lib/_shape_base_impl.py (v2.4.2)

---
+++ src/NumSharp.Core/Manipulation/np.split.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Collections.Generic;
+
+namespace NumSharp
+{
+    public static partial class np
+    {
+        /// <summary>
+        /// Split an array into multiple sub-arrays as views into ary.
+        /// </summary>
+        /// <param name="ary">Array to be divided into sub-arrays.</param>
+        /// <param name="indices_or_sections">
+        /// If an integer, N, the array will be divided into N equal arrays along axis.
+        /// If such a split is not possible, an error is raised.
+        /// </param>
+        /// <param name="axis">The axis along which to split, default is 0.</param>
+        /// <returns>A list of sub-arrays as views into ary.</returns>
+        /// <exception cref="ValueError">If indices_or_sections is an integer and does not result in equal division.</exception>
+        /// <remarks>
+        /// https://numpy.org/doc/stable/reference/generated/numpy.split.html
+        /// </remarks>
+        public static NDArray[] split(NDArray ary, int indices_or_sections, int axis = 0)
+        {
+            // Normalize axis
+            int ndim = ary.ndim;
+            if (axis < 0)
+                axis += ndim;
+            if (axis < 0 || axis >= ndim)
+                throw new ArgumentOutOfRangeException(nameof(axis), $"axis {axis} is out of bounds for array of dimension {ndim}");
+
+            int N = ary.shape[axis];
+            if (N % indices_or_sections != 0)
+                throw new ArgumentException("array split does not result in an equal division");
+
+            return array_split(ary, indices_or_sections, axis);
+        }
+
+        /// <summary>
+        /// Split an array into multiple sub-arrays as views into ary.
+        /// </summary>
+        /// <param name="ary">Array to be divided into sub-arrays.</param>
+        /// <param name="indices">
+        /// A 1-D array of sorted integers indicating where along axis the array is split.
+        /// For example, [2, 3] would result in ary[:2], ary[2:3], ary[3:].
+        /// </param>
+        /// <param name="axis">The axis along which to split, default is 0.</param>
+        /// <returns>A list of sub-arrays as views into ary.</returns>
+        /// <remarks>
+        /// https://numpy.org/doc/stable/reference/generated/numpy.split.html
+        /// </remarks>
+        public static NDArray[] split(NDArray ary, int[] indices, int axis = 0)
+        {
+            return array_split(ary, indices, axis);
+        }
+
+        /// <summary>
+        /// Split an array into multiple sub-arrays.
+        /// </summary>
+        /// <param name="ary">Array to be divided into sub-arrays.</param>
+        /// <param name="indices_or_sections">
+        /// If an integer, N, the array will be divided into N sub-arrays along axis.
+        /// If N does not divide the array equally, it returns l % n sub-arrays of size
+        /// l//n + 1 and the rest of size l//n.
+        /// </param>
+        /// <param name="axis">The axis along which to split, default is 0.</param>
+        /// <returns>A list of sub-arrays.</returns>
+        /// <remarks>
+        /// The only difference between split and array_split is that array_split allows
+        /// indices_or_sections to be an integer that does not equally divide the axis.
+        /// https://numpy.org/doc/stable/reference/generated/numpy.array_split.html
+        /// </remarks>
+        public static NDArray[] array_split(NDArray ary, int indices_or_sections, int axis = 0)
+        {
+            if (indices_or_sections <= 0)
+                throw new ArgumentException("number sections must be larger than 0.");
+
+            // Normalize axis
+            int ndim = ary.ndim;
+            if (axis < 0)
+                axis += ndim;
+            if (axis < 0 || axis >= ndim)
+                throw new ArgumentOutOfRangeException(nameof(axis), $"axis {axis} is out of bounds for array of dimension {ndim}");
+
+            int Ntotal = ary.shape[axis];
+            int Nsections = indices_or_sections;
+
+            // Calculate division points
+            // l % n sub-arrays of size l//n + 1, rest of size l//n
+            int Neach_section = Ntotal / Nsections;
+            int extras = Ntotal % Nsections;
+
+            // Build division points array
+            var div_points = new int[Nsections + 1];
+            div_points[0] = 0;
+            int cumulative = 0;
+            for (int i = 0; i < Nsections; i++)
+            {
+                // First 'extras' sections get size Neach_section + 1
+                cumulative += (i < extras) ? Neach_section + 1 : Neach_section;
+                div_points[i + 1] = cumulative;
+            }
+
+            return SplitByDivPoints(ary, div_points, Nsections, axis);
+        }
+
+        /// <summary>
+        /// Split an array into multiple sub-arrays.
+        /// </summary>
+        /// <param name="ary">Array to be divided into sub-arrays.</param>
+        /// <param name="indices">
+        /// A 1-D array of sorted integers indicating where along axis the array is split.
+        /// For example, [2, 3] would result in ary[:2], ary[2:3], ary[3:].
+        /// If an index exceeds the dimension of the array along axis, an empty sub-array
+        /// is returned correspondingly.
+        /// </param>
+        /// <param name="axis">The axis along which to split, default is 0.</param>
+        /// <returns>A list of sub-arrays.</returns>
+        /// <remarks>
+        /// https://numpy.org/doc/stable/reference/generated/numpy.array_split.html
+        /// </remarks>
+        public static NDArray[] array_split(NDArray ary, int[] indices, int axis = 0)
+        {
+            // Normalize axis
+            int ndim = ary.ndim;
+            if (axis < 0)
+                axis += ndim;
+            if (axis < 0 || axis >= ndim)
+                throw new ArgumentOutOfRangeException(nameof(axis), $"axis {axis} is out of bounds for array of dimension {ndim}");
+
+            int Ntotal = ary.shape[axis];
+            int Nsections = indices.Length + 1;
+
+            // Build division points: [0] + indices + [Ntotal]
+            var div_points = new int[Nsections + 1];
+            div_points[0] = 0;
+            for (int i = 0; i < indices.Length; i++)
+            {
+                div_points[i + 1] = indices[i];
+            }
+            div_points[Nsections] = Ntotal;
+
+            return SplitByDivPoints(ary, div_points, Nsections, axis);
+        }
+
+        /// <summary>
+        /// Internal helper to split array at given division points along an axis.
+        /// Matches NumPy's approach: swap axis to front, slice, swap back.
+        /// </summary>
+        private static NDArray[] SplitByDivPoints(NDArray ary, int[] div_points, int Nsections, int axis)
+        {
+            var sub_arys = new NDArray[Nsections];
+
+            // NumPy's approach: swap target axis to axis 0, slice, then swap back
+            // This works because slicing along axis 0 is straightforward
+            NDArray sary = swapaxes(ary, axis, 0);
+
+            for (int i = 0; i < Nsections; i++)
+            {
+                int st = div_points[i];
+                int end = div_points[i + 1];
+
+                // Build slices for axis 0 (which is the swapped target axis)
+                // We want sary[st:end, ...]
+                var slices = new Slice[sary.ndim];
+                slices[0] = new Slice(st, end);
+                for (int d = 1; d < sary.ndim; d++)
+                    slices[d] = Slice.All;
+
+                NDArray sub = sary[slices];
+
+                // Swap axis back
+                sub_arys[i] = swapaxes(sub, axis, 0);
+            }
+
+            return sub_arys;
+        }
+    }
+}
---
+++ test/NumSharp.UnitTest/Manipulation/np.split.Tests.cs
@@ -0,0 +1,316 @@
+using System;
+using AwesomeAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumSharp.UnitTest.Utilities;
+
+namespace NumSharp.UnitTest.Manipulation
+{
+    /// <summary>
+    /// Tests for np.split and np.array_split functions.
+    /// Based on NumPy 2.4.2 behavior.
+    /// </summary>
+    public class np_split_Tests : TestClass
+    {
+        #region np.split - Equal Division
+
+        [Test]
+        public void Split_1D_EqualParts()
+        {
+            // NumPy: np.split(np.arange(9), 3) -> [array([0, 1, 2]), array([3, 4, 5]), array([6, 7, 8])]
+            var a = np.arange(9);
+            var result = np.split(a, 3);
+
+            result.Length.Should().Be(3);
+            result[0].ToArray<int>().Should().ContainInOrder(0, 1, 2);
+            result[1].ToArray<int>().Should().ContainInOrder(3, 4, 5);
+            result[2].ToArray<int>().Should().ContainInOrder(6, 7, 8);
+        }
+
+        [Test]
+        public void Split_1D_UnequalDivision_ThrowsArgumentException()
+        {
+            // NumPy: np.split(np.arange(9), 4) raises ValueError
+            var a = np.arange(9);
+
+            Assert.ThrowsException<ArgumentException>(() => np.split(a, 4));
+        }
+
+        [Test]
+        public void Split_2D_Axis0()
+        {
+            // NumPy: np.split(np.arange(12).reshape(3, 4), 3, axis=0)
+            // -> [array([[0,1,2,3]]), array([[4,5,6,7]]), array([[8,9,10,11]])]
+            var d = np.arange(12).reshape(3, 4);
+            var result = np.split(d, 3, axis: 0);
+
+            result.Length.Should().Be(3);
+            result[0].Should().BeShaped(1, 4);
+            result[1].Should().BeShaped(1, 4);
+            result[2].Should().BeShaped(1, 4);
+        }
+
+        [Test]
+        public void Split_2D_Axis1()
+        {
+            // NumPy: np.split(np.arange(12).reshape(3, 4), 2, axis=1)
+            var d = np.arange(12).reshape(3, 4);
+            var result = np.split(d, 2, axis: 1);
+
+            result.Length.Should().Be(2);
+            result[0].Should().BeShaped(3, 2);
+            result[1].Should().BeShaped(3, 2);
+
+            // First result: columns 0-1
+            result[0]["0, :"].ToArray<int>().Should().ContainInOrder(0, 1);
+            result[0]["1, :"].ToArray<int>().Should().ContainInOrder(4, 5);
+
+            // Second result: columns 2-3
+            result[1]["0, :"].ToArray<int>().Should().ContainInOrder(2, 3);
+            result[1]["1, :"].ToArray<int>().Should().ContainInOrder(6, 7);
+        }
+
+        [Test]
+        public void Split_NegativeAxis()
+        {
+            // NumPy: np.split(d, 3, axis=-2) == np.split(d, 3, axis=0)
+            var d = np.arange(12).reshape(3, 4);
+            var r1 = np.split(d, 3, axis: -2);
+            var r2 = np.split(d, 3, axis: 0);
+
+            r1.Length.Should().Be(r2.Length);
+            for (int i = 0; i < r1.Length; i++)
+            {
+                ((bool)np.array_equal(r1[i], r2[i])).Should().BeTrue();
+            }
+        }
+
+        #endregion
+
+        #region np.split - With Indices
+
+        [Test]
+        public void Split_WithIndices()
+        {
+            // NumPy: np.split(a, [3, 5, 7]) -> a[:3], a[3:5], a[5:7], a[7:]
+            var a = np.arange(9);
+            var result = np.split(a, new[] { 3, 5, 7 });
+
+            result.Length.Should().Be(4);
+            result[0].ToArray<int>().Should().ContainInOrder(0, 1, 2);
+            result[1].ToArray<int>().Should().ContainInOrder(3, 4);
+            result[2].ToArray<int>().Should().ContainInOrder(5, 6);
+            result[3].ToArray<int>().Should().ContainInOrder(7, 8);
+        }
+
+        [Test]
+        public void Split_WithIndices_EmptyAtStart()
+        {
+            // NumPy: np.split(a, [0, 3, 6]) -> [], a[:3], a[3:6], a[6:]
+            var a = np.arange(9);
+            var result = np.split(a, new[] { 0, 3, 6 });
+
+            result.Length.Should().Be(4);
+            result[0].size.Should().Be(0);
+            result[1].size.Should().Be(3);
+            result[2].size.Should().Be(3);
+            result[3].size.Should().Be(3);
+        }
+
+        [Test]
+        public void Split_WithIndices_DuplicateIndices()
+        {
+            // NumPy: np.split(a, [3, 3, 6]) -> a[:3], [], a[3:6], a[6:]
+            var a = np.arange(9);
+            var result = np.split(a, new[] { 3, 3, 6 });
+
+            result.Length.Should().Be(4);
+            result[0].size.Should().Be(3);
+            result[1].size.Should().Be(0);
+            result[2].size.Should().Be(3);
+            result[3].size.Should().Be(3);
+        }
+
+        [Test]
+        public void Split_WithIndices_EmptyIndicesArray()
+        {
+            // NumPy: np.split(a, []) -> [a]
+            var a = np.arange(9);
+            var result = np.split(a, new int[] { });
+
+            result.Length.Should().Be(1);
+            result[0].size.Should().Be(9);
+        }
+
+        #endregion
+
+        #region np.array_split - Unequal Division
+
+        [Test]
+        public void ArraySplit_UnequalDivision_9Into4()
+        {
+            // NumPy: np.array_split(np.arange(9), 4)
+            // -> [array([0, 1, 2]), array([3, 4]), array([5, 6]), array([7, 8])]
+            // 9 % 4 = 1 extra, so first 1 section gets size 3, rest get size 2
+            var a = np.arange(9);
+            var result = np.array_split(a, 4);
+
+            result.Length.Should().Be(4);
+            result[0].size.Should().Be(3); // 9//4 + 1 = 3
+            result[1].size.Should().Be(2); // 9//4 = 2
+            result[2].size.Should().Be(2);
+            result[3].size.Should().Be(2);
+        }
+
+        [Test]
+        public void ArraySplit_UnequalDivision_10Into3()
+        {
+            // NumPy: np.array_split(np.arange(10), 3)
+            // -> [array([0, 1, 2, 3]), array([4, 5, 6]), array([7, 8, 9])]
+            // 10 % 3 = 1 extra, so first 1 section gets size 4, rest get size 3
+            var b = np.arange(10);
+            var result = np.array_split(b, 3);
+
+            result.Length.Should().Be(3);
+            result[0].size.Should().Be(4); // 10//3 + 1 = 4
+            result[1].size.Should().Be(3); // 10//3 = 3
+            result[2].size.Should().Be(3);
+        }
+
+        [Test]
+        public void ArraySplit_UnequalDivision_8Into3()
+        {
+            // NumPy: np.array_split(np.arange(8), 3)
+            // -> [array([0, 1, 2]), array([3, 4, 5]), array([6, 7])]
+            // 8 % 3 = 2 extras, so first 2 sections get size 3, rest get size 2
+            var c = np.arange(8);
+            var result = np.array_split(c, 3);
+
+            result.Length.Should().Be(3);
+            result[0].size.Should().Be(3); // 8//3 + 1 = 3
+            result[1].size.Should().Be(3); // 8//3 + 1 = 3
+            result[2].size.Should().Be(2); // 8//3 = 2
+        }
+
+        [Test]
+        public void ArraySplit_2D_UnequalAxis0()
+        {
+            // NumPy: np.array_split(np.arange(12).reshape(3, 4), 2, axis=0)
+            // -> [array([[0,1,2,3],[4,5,6,7]]), array([[8,9,10,11]])]
+            var d = np.arange(12).reshape(3, 4);
+            var result = np.array_split(d, 2, axis: 0);
+
+            result.Length.Should().Be(2);
+            result[0].Should().BeShaped(2, 4);
+            result[1].Should().BeShaped(1, 4);
+        }
+
+        [Test]
+        public void ArraySplit_MoreSectionsThanElements()
+        {
+            // NumPy: np.array_split(np.array([1,2,3]), 5)
+            // -> [array([1]), array([2]), array([3]), array([]), array([])]
+            var small = np.array(new[] { 1, 2, 3 });
+            var result = np.array_split(small, 5);
+
+            result.Length.Should().Be(5);
+            result[0].size.Should().Be(1);
+            result[1].size.Should().Be(1);
+            result[2].size.Should().Be(1);
+            result[3].size.Should().Be(0);
+            result[4].size.Should().Be(0);
+        }
+
+        [Test]
+        public void ArraySplit_ZeroSections_ThrowsArgumentException()
+        {
+            // NumPy: np.array_split(a, 0) raises ValueError
+            var a = np.arange(9);
+
+            Assert.ThrowsException<ArgumentException>(() => np.array_split(a, 0));
+        }
+
+        [Test]
+        public void ArraySplit_NegativeSections_ThrowsArgumentException()
+        {
+            // NumPy: np.array_split(a, -1) raises ValueError
+            var a = np.arange(9);
+
+            Assert.ThrowsException<ArgumentException>(() => np.array_split(a, -1));
+        }
+
+        #endregion
+
+        #region Edge Cases
+
+        [Test]
+        public void Split_SingleElement()
+        {
+            var single = np.array(new[] { 42 });
+            var result = np.split(single, 1);
+
+            result.Length.Should().Be(1);
+            result[0].ToArray<int>().Should().ContainInOrder(42);
+        }
+
+        [Test]
+        public void Split_EmptyArray()
+        {
+            var empty = np.array(new double[0]);
+            var result = np.split(empty, 1);
+
+            result.Length.Should().Be(1);
+            result[0].size.Should().Be(0);
+        }
+
+        [Test]
+        public void ArraySplit_EmptyArray_MultipleSections()
+        {
+            var empty = np.array(new double[0]);
+            var result = np.array_split(empty, 3);
+
+            result.Length.Should().Be(3);
+            foreach (var r in result)
+            {
+                r.size.Should().Be(0);
+            }
+        }
+
+        [Test]
+        public void Split_3D_Array()
+        {
+            // NumPy: np.split(np.arange(24).reshape(2, 3, 4), 2, axis=0)
+            var e = np.arange(24).reshape(2, 3, 4);
+            var result = np.split(e, 2, axis: 0);
+
+            result.Length.Should().Be(2);
+            result[0].Should().BeShaped(1, 3, 4);
+            result[1].Should().BeShaped(1, 3, 4);
+        }
+
+        [Test]
+        public void Split_ReturnsViews()
+        {
+            // NumPy: split returns views, not copies
+            var a = np.arange(9);
+            var result = np.split(a, 3);
+
+            // Modifying a view should affect the original
+            result[0].SetInt32(999, 0);
+            a.GetInt32(0).Should().Be(999);
+        }
+
+        [Test]
+        public void Split_DifferentDtypes()
+        {
+            // Test with float64
+            var f = np.arange(6.0);
+            var result = np.split(f, 3);
+
+            result.Length.Should().Be(3);
+            result[0].dtype.Should().Be(typeof(double));
+            result[0].ToArray<double>().Should().ContainInOrder(0.0, 1.0);
+        }
+
+        #endregion
+    }
+}
